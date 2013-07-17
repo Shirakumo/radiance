af332c4eae2941b5fb94ab590dc6cc0f8056b1a9
Shinmera <shinmera@tymoon.eu>
2013-07-17 17:39:19 +0200

    Added authentication Fixed path comparison

--- mod/core/flash-dispatch.lisp
+++ mod/core/flash-dispatch.lisp
@@ -8,15 +8,19 @@
 
 (implement 'dispatcher (get-module 'flash-dispatch))
 
-(defmethod dispatch ((dispatch flash-dispatch) request &key)
+(defmethod dispatch ((dispatch flash-dispatch) (request radiance:request) &key)
   "Dispatches a request to a module based on a subdomain."
+  (handler-case (authenticate (implementation 'auth))
+    (auth-error (err) (log:warn "~a" err))
+    (error (err) (log:error "~a" err)))
   (loop for trigger being the hash-keys of (table dispatch)
      for specific being the hash-values of (table dispatch)
      collect (destructuring-bind (subdomain domain port path) specific
                (when (and (or (not subdomain) (string-equal subdomain (concatenate-strings (subdomains request) ".")))
                           (or (not domain) (string-equal domain (domain request)))
                           (or (not port) (= port (port request)))
-                          (or (not path) (string= path (path request) :end1 (length (path request)))))
+                          (or (not path) (and (<= (length path) (length (path request)))
+                              (string= path (path request) :end2 (length path)))))
                  (trigger trigger)))
      into responses
      finally (progn (setf responses (alexandria:flatten responses))
