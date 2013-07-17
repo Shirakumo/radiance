11e1c968153e1a26be623a6e28835493c4a174b8
Shinmera <shinmera@tymoon.eu>
2013-07-17 17:41:32 +0200

    Fixed session stuff

--- mod/verify/session.lisp
+++ mod/verify/session.lisp
@@ -9,7 +9,9 @@
 (defvar *verify-sessions* (make-hash-table :test 'equal) "Session registry")
 
 (defclass verify-session (session)
-  ((user :initarg :user :initform (error "User required.") :reader user)
+  ((uuid :initarg :uuid :initform (format nil "~a" (uuid:make-v4-uuid)) :reader uuid)
+   (time :initarg :time :initform (get-unix-time) :reader session-time)
+   (user :initarg :user :initform (error "User required.") :reader user)
    (fields :initarg :fields :initform (make-hash-table :test 'equal) :reader fields)
    (active :initarg :active :initform T :accessor active)))
 
@@ -17,18 +19,26 @@
   (print-unreadable-object (session out :type T)
     (format out "~a" (user session))))
 
-(implement 'session (make-instance 'verify-session :user NIL :fields NIL :active NIL))
+(implement 'session (make-instance 'verify-session :uuid NIL :user NIL :fields NIL :active NIL))
+
+(defmethod session-get ((session verify-session) (uuid string) &key &allow-other-keys)
+  "Returns the requested session instance if applicable."
+  (gethash uuid *verify-sessions*))
+
+(defmethod session-start ((session verify-session) (user user) &key &allow-other-keys)
+  "Starts a new session for the given user, enters it in the registry and returns the session object."
+  (let ((session (make-instance 'verify-session :user user)))
+    (log:debug "Starting new session for ~a with UUID ~a" user (uuid session))
+    (setf (gethash (uuid session) *verify-sessions*) session)))
 
 (defmethod session-start ((session verify-session) (username string) &key &allow-other-keys)
   "Starts a new session for the given user, enters it in the registry and returns the session object."
-  (let ((session (gethash username *verify-sessions*)))
-    (if session session
-        (let ((session (make-instance 'verify-session :user (user-get (implementation 'user) username))))
-          (setf (gethash username *verify-sessions*) session)))))
+  (session-start session (user-get (implementation 'user) username)))
 
 (defmethod session-end ((session verify-session) &key &allow-other-keys)
   "Ends the session and removes it from the registry."
-  (remhash (username (user session)) *verify-sessions*)
+  (log:debug "Terminating session for ~a with UUID ~a" (user session) (uuid session))
+  (remhash (uuid session) *verify-sessions*)
   (setf (active session) NIL)
   session)
 
@@ -37,6 +47,10 @@
   (if value
       (setf (gethash field (fields session)) value)
       (gethash field (fields session))))
+
+(defmethod session-uuid ((session verify-session) &key &allow-other-keys)
+  "Returns the UUID of the session instance."
+  (uuid session))
 
 (defmethod session-user ((session verify-session) &key &allow-other-keys)
   "Retrieves a database instance of the corresponding user object."
