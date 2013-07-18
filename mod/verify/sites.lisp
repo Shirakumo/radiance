04ce1cacffb07361d86cabdfe8fc7250545a77c4
Shinmera <shinmera@tymoon.eu>
2013-07-18 17:16:10 +0200

    Added register page stub, changed things

--- mod/verify/sites.lisp
+++ mod/verify/sites.lisp
@@ -11,7 +11,7 @@
 (defgeneric page-logout (verify))
 (defgeneric page-register (verify))
 
-(defun get-redirect (&optional (request *radiance-request*) (default "/"))
+(defun get-redirect (&optional (default "/") (request *radiance-request*))
   (or (hunchentoot:get-parameter "redirect" request)
       (hunchentoot:post-parameter "redirect" request)
       (hunchentoot:referer request)
@@ -21,18 +21,23 @@
 
 (defmethod page-login ((verify verify))
   ($ (initialize (template "verify/login.html")))
-  (loop with target = ($ "#mechanisms")
-     for mechanism being the hash-values of *verify-mechanisms*
-     do ($ target (append (show-login mechanism))))
+  (if (or (not *radiance-session*) (session-temp-p *radiance-session*))
+      (loop with target = ($ "#mechanisms")
+         for mechanism being the hash-values of *verify-mechanisms*
+         do ($ target (append (show-login mechanism))))
+      ($ "#mechanisms" (append (parse-html "<li id=\"loggedin\"><h2>You are already logged in.</h2></li>"))))
   (first ($ (serialize :doctype "html"))))
 
 (defmethod page-auth ((verify verify))
   (let* ((name (third (split-sequence:split-sequence #\/ (path *radiance-request*))))
          (mechanism (get-mechanism (make-keyword name))))
     (if mechanism 
-        (handler-case (if (handle-login mechanism)
-                          (hunchentoot:redirect (get-redirect))
-                          (error 'auth-login-error :text "Login failed." :code 9))
+        (handler-case (let ((response (handle-login mechanism)))
+                        (if response
+                            (progn 
+                              (session-start (implementation 'session) response)
+                              (hunchentoot:redirect (get-redirect "/login")))
+                            (error 'auth-login-error :text "Login failed." :code 9)))
           (auth-error (err)
             (hunchentoot:redirect (format nil "/login?mechanism=~a&errorcode=~a&errortext=~a" name (slot-value err 'radiance::code) (slot-value err 'radiance::text)))))
         (error 'auth-error :text (format nil "Unknown authentication mechanism ~a" name) :code 8))))
@@ -41,6 +46,15 @@
   (if *radiance-session*
       (session-end *radiance-session*))
   (hunchentoot:redirect (get-redirect)))
+
+(defmethod page-register ((verify verify))
+  ($ (initialize (template "verify/register.html")))
+  (if (or (not *radiance-session*) (session-temp-p *radiance-session*))
+      (loop with target = ($ "#mechanisms")
+         for mechanism being the hash-values of *verify-mechanisms*
+         do ($ target (append (show-register mechanism))))
+      ($ "#mechanisms" (append (parse-html "<li id=\"loggedin\"><h2>You are already logged in.</h2></li>"))))
+  (first ($ (serialize :doctype "html"))))
 
 (defhook 'verify-login (get-module :verify) #'page-login)
 (defhook 'verify-auth (get-module :verify) #'page-auth)
