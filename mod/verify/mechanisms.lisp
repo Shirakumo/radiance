ea4ae5f87f4731494c6b93f89cf815ed0c5d4e6d
Shinmera <shinmera@tymoon.eu>
2013-07-21 22:35:49 +0200

    Implemented all password functionality (untested)

--- mod/verify/mechanisms.lisp
+++ mod/verify/mechanisms.lisp
@@ -6,6 +6,19 @@
 
 (in-package :radiance-mod-verify)
 
+(defun make-password-hash (user password)
+  ""
+  (let ((salt (if (config-tree :verify :password :use-per-user-salt)
+                  (user-field user "salt")
+                  (config-tree :verify :password :salt)))
+        (algorithm (or (user-field user "hash-type")
+                       (setf algorithm (config-tree :verify :password :algorithm)))))
+    (if (string-equal algorithm "pbkdf2")
+        (pbkdf2-hash password salt)
+        (if (find-symbol algorithm :ironclad)
+            (simple-hash password salt :digest (find-symbol algorithm :ironclad))
+            (error 'radiance-error :text (format nil "Unknown hashing algorithm configured: ~a" algorithm) :code 21)))))
+
 (defmechanism password
     ""
   (show-login ()
@@ -15,7 +28,13 @@
       element))
 
   (handle-login ()
-    (error 'auth-login-error :text "FOOBAR!" :code 666))
+    (let ((user (user-get (implementation 'user) (hunchentoot:post-parameter "username"))))
+      (if (user-saved-p user)
+          (let ((hash (make-password-hash user (hunchentoot:post-parameter "password"))))
+            (if (string= hash (user-field user "hash"))
+                user
+                (error 'auth-login-error :text "Invalid username or password." :code 20)))
+          (error 'auth-login-error :text "Invalid username or password." :code 20))))
   
   (show-register ()
     (let ((element (lquery:parse-html (read-data-file "template/verify/register-password.html")))
@@ -27,4 +46,14 @@
 
   (handle-link () NIL)
 
-  (handle-register () NIL))
+  (handle-register (user)
+    (let ((password (hunchentoot:post-parameter "password" *radiance-request*))
+          (pwconfirm (hunchentoot:post-parameter "pwconfirm" *radiance-request*)))
+      (if (and password pwconfirm)
+          (if (string= password pwconfirm)
+              (let ((salt (make-salt T)))
+                (user-field user "salt" :value salt)
+                (user-field user "hash-type" :value (config-tree :verify :password :algorithm))
+                (user-field user "hash-date" :value (get-unix-time))
+                (user-field user "hash" :value (make-password-hash user password)))
+              (error 'auth-register-error :text "Passwords to not match!" :code 22))))))
