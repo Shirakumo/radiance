99335356322ed7b1e88b5b6be6a7f5e1508fb75d
Shinmera <shinmera@tymoon.eu>
2013-07-24 23:28:23 +0200

    Updated flash dispatcher

--- mod/flash/flash-dispatch.lisp
+++ mod/flash/flash-dispatch.lisp
@@ -10,22 +10,26 @@
 
 (defmethod dispatch ((dispatch flash-dispatch) (request radiance:request) &key)
   "Dispatches a request to a module based on a subdomain."
-  (loop with response = NIL
-     for (trigger subdomain domain port path) in (hooks dispatch)
-     if (and (or (not subdomain) (string-equal subdomain (concatenate-strings (subdomains request) ".")))
-             (or (not domain) (string-equal domain (domain request)))
-             (or (not port) (= port (port request)))
-             (or (not path) (and (<= (length path) (length (path request)))
-                                 (string= path (path request) :end2 (length path)))))
-     do (setf response (trigger trigger))
-     finally (return (or response (read-data-file "static/html/hello.html")))))
-
+  (or 
+   (loop for (trigger subdomain domain port path) in (hooks dispatch)
+      if (and (or (not subdomain) (string-equal subdomain (concatenate-strings (subdomains request) ".")))
+              (or (not domain) (string-equal domain (domain request)))
+              (or (not port) (= port (port request)))
+              (or (not path) (and (<= (length path) (length (path request)))
+                                  (string= path (path request) :end2 (length path)))))
+      do (return (trigger trigger)))
+   (trigger :dispatch-default)))
+   
 (defmethod register ((dispatch flash-dispatch) trigger &key subdomain domain port path)
   "Registers a subdomain for a module. If the subdomain is NIL, all unhandled requests are dispatched to the module."
   (setf (hooks dispatch) 
         (sort (append (hooks dispatch) `((,trigger ,subdomain ,domain ,port ,path)))
               #'sort-dispatcher-hooks))
   trigger)
+
+(defmethod dispatch-default ((dispatch flash-dispatch))
+  (read-data-file "static/html/hello.html"))
+(defhook :dispatch-default (get-module 'flash-dispatch) #'dispatch-default)
 
 (defun count-path (path)
   (if (or (eq path NIL) (equal path "/")) 0
