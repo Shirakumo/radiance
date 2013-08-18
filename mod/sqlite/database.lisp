6654ccf6a075249f4f6a24609756a6140cb9f5b4
Shinmera <shinmera@tymoon.eu>
2013-08-18 17:05:14 +0200

    Added sqlite database skeleton.

--- mod/sqlite/database.lisp
+++ mod/sqlite/database.lisp
@@ -5,3 +5,42 @@
 |#
 
 (in-package :radiance-mod-sqlite)
+
+(implement 'database (get-module 'sqlite))
+
+(defmethod db-connect ((db sqlite) dbname &key (root-path (merge-pathnames "data/" (pathname (config :root)))))
+  (log:info "Connecting to SQLite database ~a on ~a" dbname root-path)
+  (setf (dbinstance db)
+        (sqlite:connect (merge-pathnames dbname root-path))))
+
+(defmethod db-disconnect ((db sqlite) &key)
+  (log:info "Disconnecting...")
+  (sqlite:disconnect (dbinstance db))
+  (setf (dbinstance db) NIL))
+
+(defmethod db-connected-p ((db sqlite) &key)
+  (not (null (dbinstance db))))
+
+(defmethod db-collections ((db sqlite) &key)
+  )
+
+(defmethod db-create ((db sqlite) (collection string) fields &key indices)
+  )
+  
+(defmethod db-select ((db sqlite) (collection string) query &key (skip 0) (limit 0) sort) 
+  )
+
+(defmethod db-iterate ((db sqlite) (collection string) query function &key (skip 0) (limit 0) sort)
+  )
+
+(defmethod db-insert ((db sqlite) (collection string) data) 
+  )
+
+(defmethod db-remove ((db sqlite) (collection string) query &key (skip 0) (limit 0)) 
+  )
+
+(defmethod db-update ((db sqlite) (collection string) query data &key (skip 0) (limit 0) replace) 
+  )
+
+(defmethod db-apropos ((db sqlite) (collection string))
+  )
