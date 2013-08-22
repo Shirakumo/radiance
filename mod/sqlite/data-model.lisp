313538933bda8bbe5755b4e766a078ec87bc4fac
Shinmera <shinmera@tymoon.eu>
2013-08-22 14:30:00 +0200

    Finished all functions, untested.

--- mod/sqlite/data-model.lisp
+++ mod/sqlite/data-model.lisp
@@ -8,9 +8,7 @@
 
 (defclass sqlite-data-model (data-model)
   ((document :initarg :document :initform (make-hash-table :test 'equal) :accessor document)
-   (delta :initarg :delta :initform (make-hash-table :test 'equal) :accessor delta)
-   (collection :initarg :collection :initform (error "Collection required") :accessor collection)
-   (hull-p :initarg :hull-p :initform T :accessor hull-p))
+   (collection :initarg :collection :initform (error "Collection required") :accessor collection))
   (:documentation "Datamodel for sqlite."))
 
 (defmethod print-object ((model sqlite-data-model) out)
@@ -23,15 +21,17 @@
 
 (defmethod model-field ((model sqlite-data-model) (field string) &key (value NIL v-p))
   (if v-p
-      (setf (gethash field (delta model)) value)
-      (or (gethash field (delta model))
-          (gethash field (document model)))))
+      (setf (gethash field (document model)) value)
+      (gethash field (document model))))
 
 (defun model-field-set (model field value)
   "Set the value of a field in the document."
-  (setf (gethash field (delta model)) value))
+  (setf (gethash field (document model)) value))
 
 (defsetf model-field model-field-set)
+
+(defmethod model-id ((model sqlite-data-model) &key)
+  (gethash "_id" (document model)))
 
 (defmethod model-get ((model sqlite-data-model) (collection string) query &key (skip 0) (limit -1) sort)
   (multiple-value-bind (where-part queryargs) (query-to-where-part query)
@@ -75,25 +75,35 @@
           NIL))))
           
 (defmethod model-hull ((model sqlite-data-model) (collection string) &key)
-  (make-instance 'sqlite-data-model :collection collection :hull-p T))
+  (make-instance 'sqlite-data-model :collection collection))
 
 (defmethod model-hull-p ((model sqlite-data-model) &key) 
-  (hull-p model))
+  (eq (gethash "_id" (document model)) NIL))
 
 (defmethod model-save ((model sqlite-data-model) &key)
-  (multiple-value-bind (set-part values2) (model-set-part (delta model))
-    (multiple-value-bind (where-part values1) (model-where-part (document model))
-      (db-query (get-module :sqlite) (format NIL "UPDATE ~a ~a ~a LIMIT 1;" (collection model) set-part where-part) (append values1 values2))
-      (setf (document model) (delta model)
-            (delta model) (make-hash-table :test 'equal)
-            (hull-p model) NIL))))
+  (assert (not (model-hull-p model)) () "Model has not been inserted before.")
+  (multiple-value-bind (set-part values) (model-set-part (document model))
+    (db-query (get-module :sqlite) (format NIL "UPDATE ~a ~a WHERE _id = ? LIMIT 1;" (collection model) set-part) (append values (list (model-id model))))
+    (setf (hull-p model) NIL)))
 
 (defmethod model-delete ((model sqlite-data-model) &key)
-  (multiple-value-bind (where-part values) (model-where-part (delta model))
-    (db-query (get-module :sqlite) (format NIL "DELETE FROM ~a ~a LIMIT 1;" (collection model) where-part) values)))
+  (assert (not (model-hull-p model)) () "Model has not been inserted before.")
+  (db-query (get-module :sqlite) (format NIL "DELETE FROM ~a WHERE _id = ? LIMIT 1;" (collection model)) (list (model-id model))))
 
 (defmethod model-insert ((model sqlite-data-model) &key clone)
-  )
+  (let ((mod (get-module :sqlite)))
+    (loop for key being the hash-keys of (document model)
+       for val being the hash-values of (document model)
+       collect key into keys
+       collect val into vals
+       finally (db-query mod (format NIL "INSERT INTO ~a (~{~a~^, ~}) VALUES (~{?~*~^, ~})" (collection model) keys vals) vals))
+    (let ((id (sqlite:last-insert-rowid (dbinstance mod))))
+      (if clone
+          (setf model (make-instance 'sqlite-data-model 
+                                     :collection (collection model)
+                                     :document (alexandria:copy-hash-table (document model)))))
+      (setf (gethash "_id" (document model)) id)
+      model)))
 
 (defun model-where-part (document)
   (loop for where-key being the hash-keys of document
