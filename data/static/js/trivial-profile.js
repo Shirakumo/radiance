73403bb378de1dac479296ebdf20e84ba0108b22
Shinmera <shinmera@tymoon.eu>
2013-08-26 13:01:06 +0200

    Updated css files and added trivial-profile profile template draft.

---
+++ data/static/js/trivial-profile.js
@@ -0,0 +1,61 @@
+function getNowAsDateString(){
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth() + 1;
+    var y = date.getFullYear();
+    return '' + y + '.' + (m<=9 ? '0' + m : m) + '.' + (d <= 9 ? '0' + d : d);
+}
+
+var primaryColor = "rgb(0, 136, 238)";
+var targetColor = "rgb(0, 136, 238)";
+
+function customizeProfile(color, image){
+    function change_color($this, style){
+        if($this.css(style) == primaryColor){
+            $this.css(style, targetColor);
+        }
+    }
+
+    if(image){
+        $("#profile").css("background", "url('" + image + "')");
+    }
+    
+    if(color){
+        targetColor = color;
+        $("*").each(function(){
+            $this = $(this);
+            change_color($this, "border-left-color");
+            change_color($this, "border-right-color");
+            change_color($this, "border-top-color");
+            change_color($this, "border-bottom-color");
+            change_color($this, "background-color");
+            change_color($this, "color");
+        });
+        primaryColor = targetColor;
+    }
+}
+
+$(function(){
+    $("header#topmenu li").hover(function(){
+        $(this).children("ul").stop(true, true).fadeIn(200);
+    }, function (){
+        $(this).children("ul").stop(true, true).fadeOut(200);
+    });
+
+    $("#profile-comments-submit textarea").css("min-height", "10px").click(function(){
+        $(this).css("height", "100px");
+    });
+
+    $("#profile-comments-submit input[type=\"submit\"]").click(function(){
+        $form = $("#profile-comments-submit");
+        $textarea = $("#profile-comments-submit textarea");
+        $clone = $("#profile-comments ul li").first().clone();
+        $("header img", $clone).attr("src", $("header img", $form).attr("src"));
+        $("header a", $clone).text($("header a", $form).text());
+        $(".date", $clone).text(getNowAsDateString());
+        $(".text", $clone).html("<p>" + $textarea.val() + "</p>");
+        $clone.prependTo("#profile-comments ul").hide().fadeIn();
+        $("#profile-comments-submit textarea").val("");
+        return false;
+    });
+});
