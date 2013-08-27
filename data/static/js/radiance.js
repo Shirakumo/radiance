2541de8a4e3e1ce1ce433af3d8b5da627c9d1a0e
Shinmera <shinmera@tymoon.eu>
2013-08-27 17:09:24 +0200

    Updated pages. Added radiance.js file to gather common js functions.

---
+++ data/static/js/radiance.js
@@ -0,0 +1,32 @@
+function pad(string, n, padding){
+    if(n == undefined) n = 2;
+    if(padding == undefined) padding = " ";
+    var truen = n-(string+"").length;
+    return new Array(truen+1).join(padding) + string;
+}
+
+function getDateAsDateString(date){
+    if(date == undefined) date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth() + 1;
+    var y = date.getFullYear();
+    return '' + y + '.' + pad(m, 2, "0") + '.' + pad(d, 2, "0");
+}
+
+function getDateAsDateTimeString(date){
+    if(date == undefined) date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    return getDateAsDateString(date) + ' ' + pad(h, 2, "0") + ':' + pad(m, 2, "0") + ':' + pad(s, 2, "0");
+}
+
+
+$(function(){
+    $(".unixtime").each(function(){
+        $this = $(this);
+        $this.text(getDateAsDateTimeString(new Date(parseInt($this.text())*1000)));
+    });
+
+
+});
