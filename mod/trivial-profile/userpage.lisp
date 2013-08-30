ed72e644902717927aac392af5e95d4e3a6fdd0e
Shinmera <shinmera@tymoon.eu>
2013-08-30 19:11:30 +0200

    Primitive working version of user profiles.

--- mod/trivial-profile/userpage.lisp
+++ mod/trivial-profile/userpage.lisp
@@ -6,5 +6,12 @@
 
 (in-package :radiance-mod-trivial-profile)
 
-(defpage profile #u"user./" (:lquery (template "trivial-profile/profile.html"))
-  )
+(defpage profile #u"user./" (:lquery T)
+  (let ((user (user-get T (path *radiance-request*))))
+    (if (user-saved-p user)
+        (progn
+          ($ (initialize (template "trivial-profile/profile.html")))
+          (uibox:fill-foreach (user-get-actions user 10 :public T) "#profile-actions ul li")
+          (uibox:fill-foreach (model-get T "trivial-profile-comments" (query (:= "user" (path *radiance-request*))) :limit -1 :sort '(("time" . :DESC))) "#profile-comments ul li")
+          ($ "*[data-uibox]" (each #'(lambda (node) (uibox:fill-node node user)))))
+        (error-page 404))))
