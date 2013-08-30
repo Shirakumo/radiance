0b383bf0668cc7f5f1d8e3c95dc37d1658ea0c62
Shinmera <shinmera@tymoon.eu>
2013-08-30 20:05:43 +0200

    Fixed table schema, added submission api.

--- mod/trivial-profile/comments.lisp
+++ mod/trivial-profile/comments.lisp
@@ -7,5 +7,9 @@
 (in-package :radiance-mod-trivial-profile)
 
 (defmethod init-comments-db ((module trivial-profile))
-  (db-create T "trivial-profile-comments" '(("user" :varchar 32) ("time" :integer) ("text" :text)) :indices '("user")))
+  (db-create T "trivial-profile-comments" '(("user" :varchar 32) ("author" :varchar 32) ("time" :integer) ("text" :text)) :indices '("user")))
 (defhook :server :init (get-module :trivial-profile) #'init-comments-db)
+
+(defapi comment (text user) (:method :POST :access-branch "user.comment")
+  (db-insert T "trivial-profile-comments" `(("user" . ,user) ("author" . ,(user-field (user) "username")) ("time" . ,(get-unix-time)) ("text" . ,text)))
+  (redirect (concatenate 'string "/" user)))
