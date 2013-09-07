fcd49df4db2302d627c3a7026a6dd4c02925a411
Shinmera <shinmera@tymoon.eu>
2013-09-07 21:05:41 +0200

    Start of db-introspect module.

---
+++ mod/db-introspect/admin.lisp
@@ -0,0 +1,50 @@
+#|
+  This file is a part of TyNETv5/Radiance
+  (c) 2013 TymoonNET/NexT http://tymoon.eu (shinmera@tymoon.eu)
+  Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package :radiance-mod-db-introspect)
+
+(define-admin-panel database database (:access-branch "admin.database.*" :menu-icon "icon-calendar" :menu-tooltip "Show all collections in the database" :lquery (template "db-introspect/database.html"))
+  (uibox:fill-foreach (loop for collection in (db-collections T)
+                         collect `(:name ,collection :records ,(length (db-select T collection :all :limit -1)))) "tbody tr")
+  )
+
+(define-admin-panel collection database (:access-branch "admin.database.collection.*" :menu-icon "icon-table" :menu-tooltip "View collection contents" :lquery (template "db-introspect/collection.html"))
+  (let ((selected (or (post-var "selected")
+                      (list (get-var "name")))))
+    (if selected
+        (if (string= (post-or-get-var "action") "delete")
+            NIL
+            (display-collection (first selected)))
+        (redirect))))
+
+(defun display-collection (name)
+  (let ((fields (db-apropos T name)))
+    (loop with template = ($ "thead .template" (node))
+       for name in fields
+       collect ($ template (clone) (node) (text name)) into nodes
+       finally (progn ($ nodes (insert-before template))
+                      ($ template (remove))))
+    (let* ((template ($ "tbody tr" (node)))
+           (rows (db-iterate
+                  T name :all
+                  #'(lambda (record)
+                      (loop with row = ($ template (clone) (node))
+                         with inner-template = ($ row ".template" (node))
+                         for name in fields
+                         collect ($ inner-template (clone) (node) (text (cdr (assoc name record :test #'string-equal)))) into nodes
+                         finally (progn ($ nodes (insert-before inner-template))
+                                        ($ inner-template (remove))
+                                        (uibox:fill-node row record)
+                                        (return row)))))))
+      ($ rows (insert-before template))
+      ($ template (remove)))))
+
+(define-admin-panel record database (:access-branch "admin.database.collection.record.*" :menu-icon "icon-list-alt" :menu-tooltip "View record contents" :lquery (template "db-introspect/record.html"))
+  (let ((selected (or (post-var "selected")
+                      (list (get-var "_id")))))
+    (if selected
+        NIL
+        (redirect))))
---
+++ mod/db-introspect/db-introspect.mod
@@ -0,0 +1,22 @@
+#|
+  This file is a part of TyNETv5/Radiance
+  (c) 2013 TymoonNET/NexT http://tymoon.eu (shinmera@tymoon.eu)
+  Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(defpackage org.tymoonnext.radiance.mod.db-introspect
+  (:nicknames :radiance-mod-db-introspect)
+  (:use :cl :radiance :lquery))
+(in-package :radiance-mod-db-introspect)
+
+(defmodule db-introspect ()
+  "Provides admin panels to manipulate the database."
+  (:fullname "Database Introspect"
+   :author "Nicolas Hafner"
+   :version "0.0.1"
+   :license "Artistic"
+   :url "http://tymoon.eu"
+   :dependencies '(admin)
+   :implements '())
+  
+  (:components ((:file "admin"))))
