bd5d7cefa3882562a29b85ef9891e8e80d2dbfe6
Shinmera <shinmera@tymoon.eu>
2013-10-23 11:21:03 +0200

    Fixed continuation retrieval and UUID setting.

--- lib/core/continuation.lisp
+++ lib/core/continuation.lisp
@@ -7,7 +7,7 @@
 (in-package :radiance)
 
 (defclass request-continuation ()
-  ((id :initarg :id :initform (uuid:make-v4-uuid) :reader id)
+  ((id :initarg :id :initform (format NIL "~a" (uuid:make-v4-uuid)) :reader id)
    (name :initarg :name :initform "CONT" :accessor name)
    (request :initarg :request :initform (error "Request required.") :reader request)
    (function :initarg :function :initform (error "Function required.") :reader continuation-function))
@@ -21,9 +21,9 @@
   (if (and session (session-field session 'CONTINUATIONS))
       (gethash id (session-field session 'CONTINUATIONS))))
 
-(defun make-continuation (function &key (id (uuid:make-v4-uuid)) (name "CONT") (request *radiance-request*) (session *radiance-session*))
+(defun make-continuation (function &key (id (format NIL "~a" (uuid:make-v4-uuid))) (name "CONT") (request *radiance-request*) (session *radiance-session*))
   (if (null (session-field session 'CONTINUATIONS))
-      (setf (session-field session 'CONTINUATIONS) (make-hash-table)))
+      (setf (session-field session 'CONTINUATIONS) (make-hash-table :test 'equal)))
   (setf (gethash id (session-field session 'CONTINUATIONS))
         (make-instance 'request-continuation
                        :name name :id id
@@ -31,15 +31,16 @@
                        :function function)))
 
 ;; Macro to build request continuations
-(defmacro with-request-continuation ((&key (request *radiance-request*) (new-request-var (gensym "NEW-REQUEST")) (name "CONT") (session *radiance-session*)) &body continuation)
+(defmacro with-request-continuation ((&key (request '*radiance-request*) (session '*radiance-session*) (new-request-var (gensym "NEW-REQUEST")) (name "CONT")) &body continuation)
   "Builds a request-continuation and returns the generated continuation ID."
   (let ((funcsym (gensym "FUNCTION"))
         (contsym (gensym "CONTINUATION"))
         (reqsym (gensym "OLD-REQUEST")))
-    `(let* ((,reqsym request)
+    `(let* ((,reqsym ,request)
             (,funcsym (lambda () 
                         (let ((,new-request-var *radiance-request*)
                               (*radiance-request* ,reqsym))
+                          (declare (ignorable ,new-request-var))
                           ,@continuation)))
             (,contsym (make-continuation ,funcsym :name ,name :request ,request :session ,session)))
        (id ,contsym))))
