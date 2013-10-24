731e07f1232c20130f537fcd947b40108e012eaa
Shinmera <shinmera@tymoon.eu>
2013-10-24 10:43:16 +0200

    Re-added notice, fixed confirm.

--- mod/uibox/build.lisp
+++ mod/uibox/build.lisp
@@ -27,20 +27,35 @@ Test is the test function to compare choice values against the selected argument
          ($ node (append option)))
     node))
 
+(defun notice (message &key (type :ok) classes id (prepend T))
+  "Builds a notice element and prepends it to the document.
+If prepend is NIL, the notice node is returned instead."
+  (let ((node (lquery:parse-html "<div></div>")))
+    ($ node (text message))
+    ($ node (attr :class (format NIL "notice ~a ~{~a~^ ~}" (string-downcase type) classes)))
+    (if id ($ node (attr :id id)))
+    (if prepend ($ (prepend node)))
+    (first node)))
+
 (defmacro confirm ((message &key (yes "Yes") (no "No") (type :question) classes (confirm-field (gensym "CONFIRM"))) yes-block no-block)
   "Macro to build an intermittant confirm screen."
   (let ((session-field (gensym "REQUEST"))
-        (confirm-field (symbol-name confirm-field)))
+        (confirm-field (symbol-name confirm-field))
+        (rcidsym (gensym "RCID"))
+        (requestsym (gensym "REQUEST")))
     `(let ((*radiance-session* (or (session-field *radiance-session* ',session-field)
-                                   *radiance-session*)))
-       (cond
-         ((string= (get-var ,confirm-field) ,yes)
-          ,yes-block)
-         ((string= (get-var ,confirm-field) ,no)
-          ,no-block)
-         (T
-          ($ (initialize (template "uibox/confirm.html")))
-          ($ "#message" (text ,message))
-          ($ ".confirm" (attr :class ,(format NIL "~a ~{~a~^ ~}" (string-downcase type) classes)))
-          ($ ".yes" (attr :name ,confirm-field :value ,yes))
-          ($ ".no" (attr :name ,confirm-field :value ,no)))))))
+                                   *radiance-session*))
+           (,rcidsym (with-request-continuation (:new-request-var ,requestsym)
+                       (set-content-type "text/plain")
+                       (cond
+                         ((string= (get-var ,confirm-field ,requestsym) ,yes)
+                          ,yes-block)
+                         ((string= (get-var ,confirm-field ,requestsym) ,no)
+                          ,no-block)
+                         (T "WTF?")))))
+       ($ (initialize (template "uibox/confirm.html")))
+       ($ "#message" (text ,message))
+       ($ ".rcid" (attr :value ,rcidsym))
+       ($ ".confirm" (attr :class ,(format NIL "~a ~{~a~^ ~}" (string-downcase type) classes)))
+       ($ ".yes" (attr :name ,confirm-field :value ,yes))
+       ($ ".no" (attr :name ,confirm-field :value ,no)))))
