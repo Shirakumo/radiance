824bcac6614d9872b4cb719e5988b5b37f3ec2eb
Shinmera <shinmera@tymoon.eu>
2013-12-29 15:36:32 +0100

    Added asdf system classes.

--- lib/core/module.lisp
+++ lib/core/module.lisp
@@ -6,211 +6,22 @@
 
 (in-package :radiance)
 
-(defmacro make-module-class (name superclasses docstring &optional extra-slots)
-  `(defclass ,name ,superclasses
-     ((name :initarg :name :reader name :type string :allocation :class)
-      (author :initarg :author :reader author :type string :allocation :class)
-      (version :initarg :version :reader version :type string :allocation :class)
-      (license :initarg :license :reader license :type string :allocation :class)
-      (url :initarg :url :reader url :type string :allocation :class)
-      (description :initarg :description :reader description :type string :allocation :class)
-      
-      (collections :initarg :collections :reader collections :type list :allocation :class)
-      (persistent :initform T :initarg :persistent :reader persistent :type boolean :allocation :class)
-                            
-      (implements :initarg :implements :reader implementations :type list :allocation :class)
-      (asdf-system :initarg :asdf-system :reader asdf-system :type symbol :allocation :class)
-      (dependencies :initarg :dependencies :reader dependencies :type list :allocation :class)
-      (compiled :initarg :compiled :accessor compiled-p :type boolean :allocation :class)
-      ,@extra-slots)
-     (:documentation ,docstring)))
+(defclass interface (asdf:system)
+  ((%interface-name :initarg :interface-name :initform (error "INTERFACE-NAME required.") :accessor interface-name)))
 
-(make-module-class module () "Radiance base module class")
+(defclass module (asdf:system)
+  ((%implement :initarg :implement :initform () :accessor implement)))
 
-(defmacro defmodule (name superclasses docstring 
-                     (&key fullname author version license url collections (persistent T) implements asdf-system dependencies compiled)
-                     (&body defsystem)
-                     &body extra-slots)
-  "Define a new Radiance module."
-  (if (getf defsystem :depends-on)
-      (nappend (getf defsystem :depends-on) (list :radiance-lib-core))
-      (setf (getf defsystem :depends-on) (list :radiance-lib-core)))
-  (let ((superclasses (if (not superclasses) '(module) superclasses))
-        (classdef (gensym "CLASSDEF"))
-        (initializer (gensym "INITIALIZER"))
-        (asdf-system (or asdf-system (format nil "RADIANCE-MOD-~a" name))))
-    `(eval-when (:compile-toplevel :load-toplevel :execute) 
-       
-       ,(if defsystem
-            `(asdf:defsystem ,(intern (string-upcase asdf-system))
-               :name ,fullname
-               :license ,license
-               :author ,author
-               :version ,version
-               :description ,docstring
-               ,@defsystem))
-
-       (flet ((,classdef () (v:debug :radiance.server.module "Defining module ~a" ',name) (make-module-class ,name ,superclasses ,docstring ,extra-slots))
-              (,initializer () (v:debug :radiance.server.module "Initializing module ~a" ',name)
-                            (let ((instance (make-instance ',name 
-                                                       :name ,fullname :author ,author :version ,version :license ,license :url ,url :description ,docstring
-                                                       :collections ,collections :persistent ,persistent
-                                                       :implements ,implements :asdf-system ,asdf-system :dependencies ,dependencies
-                                                       :compiled ,compiled)))
-                              (restart-case
-                                  (progn 
-                                    (when (and (not (eql (class-of instance)
-                                                         (class-of (get-module T))))
-                                               (get-module T))
-                                      (error 'module-already-initialized :module (get-module T)))
-                                    (setf (get (package-symbol *package*) :module) instance)
-                                    (setf (gethash (make-keyword ',name) *radiance-modules*) instance))
-                                (override-existing-module ()
-                                  :report "Override the existing module in the package (could break things!)"
-                                  (setf (get (package-symbol *package*) :module) instance)
-                                  (setf (gethash (make-keyword ',name) *radiance-modules*) instance))))))
-         
-         (restart-case (if (gethash (make-keyword ',name) *radiance-modules*)
-                           (error 'module-already-initialized :module ',name)
-                           (progn (,classdef) (,initializer)))
-           (override-both () 
-             :report "Redefine the module and create a new instance of the module anyway."
-             (,classdef) (,initializer))
-           (override-module ()
-             :report "Just redefine the module."
-             (,classdef))
-           (override-instance ()
-             :report "Just create a new instance of the module."
-             (,initializer))
-           (do-nothing ()
-             :report "Leave module and instance as they are."))))))
-
-(defmethod print-object ((mod module) out)
-  (print-unreadable-object (mod out :type t)
-    (if (version mod) (format out "v~a" (version mod)))))
-
-(defgeneric init (module)
-  (:documentation "Called when Radiance is started up."))
-
-(defgeneric shutdown (module)
-  (:documentation "Called when Radiance is shut down."))
-
-(defgeneric get-module (module)
-  (:documentation "Retrieves the requested module from the instance list."))
-
-(defmethod get-module ((module module)) module)
-
-(defmethod get-module ((module symbol))
-  "Retrieves the requested module from the instance list."
-  (if (eq (symbol-package module) (find-package :keyword))
-      (gethash module *radiance-modules*)
-      (get-module (symbol-name module))))
-
-(defmethod get-module ((module string))
-  "Retrieves the requested module from the instance list."
-  (get-module (make-keyword module)))
-
-(defmethod get-module ((module (eql T)))
-  "Retrieves the module of the current package, if any."
-  (get (package-symbol *package*) :module))
-
-(defun module-package (module)
-  "Retrieve the package of the module."
-  (symbol-package (class-name (class-of (get-module module)))))
-
-(defgeneric module-symbol (module)
-  (:documentation "Gets the symbol to access the given module."))
-
-(defmethod module-symbol ((module string))
-  (find-symbol (string-upcase module) :keyword))
-
-(defmethod module-symbol ((module module))
-  (module-symbol (class-name (class-of module))))
-
-(defmethod module-symbol ((module symbol))
-  (if (eq (symbol-package module) (find-package :keyword))
-      module
-      (module-symbol (symbol-name module))))
-
-(defun discover-modules (&key redefine reinitialize)
-  (walk-directory (merge-pathnames "mod/" (pathname (config :root)))
-                         (lambda (file) (load-module file :redefine redefine :reinitialize reinitialize))
-                         :test (lambda (file) 
-                                 (let ((name (file-namestring file)))
-                                   (equal ".mod" (subseq name (- (length name) 4)))))
-                         :first :FILES))
-
-(defun load-module (file &key redefine reinitialize &allow-other-keys)
-  (v:debug :radiance.server.module "Discovered mod file: ~a (~a)" file (file-namestring file))
-  (handler-bind ((module-already-initialized
-                  #'(lambda (c) (v:warn :radiance.server.module "Error loading module: ~a" c)
-                            (cond
-                              ((not (or redefine reinitialize)) (invoke-restart 'do-nothing))
-                              ((and redefine reinitialize) (invoke-restart 'override-both))
-                              (redefine (invoke-restart 'override-module))
-                              (reinitialize (invoke-restart 'override-instance))))))
-    (load file)))
-
-(defgeneric compile-module (module &key force)
-  (:documentation "Compiles a module's source files."))
-
-(defmethod compile-module ((module module) &key force)
-  (when (or (not (slot-value module 'compiled)) force)
-    (v:debug :radiance.server.module "Compiling module ~a (~a)" module (asdf-system module))
-    (loop for dependency in (slot-value module 'dependencies)
-       do (compile-dependency dependency :force force))
-    (ql:quickload (asdf-system module) :verbose NIL)
-    (setf (slot-value module 'compiled) T)))
-
-(defmethod compile-module ((module string) &key force)
-  (let ((module-instance (get-module module)))
-    (if module-instance
-        (compile-module module-instance :force force)
-        (error "Module ~a not found." module))))
-
-(defmethod compile-module ((module symbol) &key force)
-  (compile-module (symbol-name module) :force force))
-
-(defun compile-dependency (dependency &key force)
-  (cond
-    ((get-module dependency) (compile-module dependency :force force))
-    ((config-tree :implementations (make-keyword dependency)) 
-     (let ((deps (config-tree :implementations (make-keyword dependency))))
-       (if (listp deps)
-           (loop for dep in deps do (compile-module dep :force force))
-           (compile-module deps :force force))))
-    (T (error "Dependency ~a not found!" dependency))))
-
-(defun compile-modules (&key force)
-  (dolist (module (config :modules))
-    (compile-module module :force force)))
-
-(defclass column ()
-  ((name :initform (error "Column name required.") :initarg :name :reader name :type string)
-   (access-mode :initform "000" :initarg :access-mode :reader access-mode :type string)
-   (description :initform NIL :initarg :description :reader description :type string))
-  (:documentation "Abstract database column class for metadata purposes."))
-
-(defclass collection (column)
-  ((columns :initform (error "List of columns required.") :initarg :columns :reader columns :type simple-vector))
-  (:documentation "Abstract database collection class for metadata purposes."))
-
-(defmethod print-object ((col column) out)
-  (print-unreadable-object (col out :type t)
-    (format out "~a (~a)" (name col) (access-mode col))))
-
-(defun make-column (name &key (access-mode "000") description)
-  "Shorthand function to create a new column instance."
-  (make-instance 'column :name name :access-mode access-mode :description description))
-
-(defun make-collection (name &key (access-mode "000") description columns)
-  "Create a new representation of a collection."
-  (make-instance 'collection :name name :access-mode access-mode :description description
-                 :columns (loop with array = (make-array (length columns) :element-type 'column :fill-pointer 0)
-                             for column in columns
-                             do (vector-push (if (listp column) 
-                                                 (destructuring-bind (name &optional mode description) column
-                                                   (make-column name :access-mode mode :description description))
-                                                 (make-column column)) array)
-                             finally (return array))))
+(defmethod asdf:operate ((op asdf:load-op) (interface interface) &key)
+  (let* ((name (interface-name interface))
+         (implementation (config-tree :implementation name)))
+    (if implementation
+        (let ((system (asdf:find-system implementation)))
+          (asdf:load-system system)
+          (let ((package (find-package name)))
+            (if package
+                (setf (symbol-value (find-symbol "*IMPLEMENTATION*" package))
+                      (make-instance (cdr (assoc name (implement system)))))
+                (error 'no-such-interface-error :interface name))))
+        (error 'no-interface-implementation-error :interface name))))
 
