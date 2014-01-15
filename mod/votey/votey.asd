20c2111d49834ea79921f882e06c17aaf79b9b92
Shinmera <shinmera@tymoon.eu>
2014-01-16 00:10:49 +0100

    Fixed up votey.

--- mod/votey/votey.asd
+++ mod/votey/votey.asd
@@ -1,3 +1,9 @@
+#|
+This file is a part of TyNETv5/Radiance
+(c) 2013 TymoonNET/NexT http://tymoon.eu (shinmera@tymoon.eu)
+Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
 (defpackage org.tymoonnext.radiance.mod.votey
   (:use :cl :radiance :lquery)
   (:nicknames :radiance-mod-votey))
--- mod/votey/votey.lisp
+++ mod/votey/votey.lisp
@@ -1,17 +1,23 @@
+#|
+This file is a part of TyNETv5/Radiance
+(c) 2013 TymoonNET/NexT http://tymoon.eu (shinmera@tymoon.eu)
+Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
 (in-package :radiance-mod-votey)
 
 ;; Populate initial database
-(unless (db-select T "votey-options" :all)
+(unless (db:select "votey-options" :all)
   (loop for option in '("Yes" "No" "Neither" "Both" "Ech")
      for i = 0 then (1+ i)
-     do (db-insert T "votey-options"
+     do (db:insert "votey-options"
                    (acons "id" i 
                    (acons "votes" 0 
                    (acons "title" option ()))))))
   
 
 (defpage vote #u"vote./" (:lquery (template "votey.html"))
-  (let ((options (model-get T "votey-options" :all)))
+  (let ((options (dm:get "votey-options" :all)))
     (if options
         (uibox:fill-foreach options "#options")
         ($ "#error" (text "No options available!"))))
@@ -22,10 +28,10 @@
   ($ "#ok" (text (get-var "ok"))))
 
 (defapi vote (option) (:access-branch "*")
-  (let ((option (model-get-one T "votey-options" (:= "id" (parse-integer option)))))
+  (let ((option (dm:get-one "votey-options" (:= "id" (parse-integer option)))))
     (if option
-        (progn (setf (model-field option "votes") 
-                     (1+ (model-field option "votes")))
-               (model-save option)
+        (progn (setf (dm:field option "votes") 
+                     (1+ (dm:field option "votes")))
+               (dm:save option)
                (redirect "/?ok=Vote registered!"))
         (redirect "/?error=No such option!"))))
