53ffb19f4a17a0714a76e806429afc04e57dc6be
Shinmera <shinmera@tymoon.eu>
2014-01-23 20:13:41 +0100

    Minor changes.

--- mod/flash/flash-dispatch.lisp
+++ mod/flash/flash-dispatch.lisp
@@ -11,25 +11,26 @@
 (define-interface-method dispatcher:dispatch (request)
   (or
    (let ((hook (dispatcher:effective-trigger request)))
-     (if hook (funcall (item-function hook))))
+     (when hook
+       (v:debug :flash-dispatch "Dispatching to hook: ~a" hook)
+       (funcall (item-function hook))))
    (trigger :server :dispatch-default)
    (dispatcher:dispatch-default request)))
 
 (define-interface-method dispatcher:effective-trigger (request)
   (loop for (hook identifier uri) in *hooks*
-     if (uri-matches uri request)
-     do (return (loop for item in (hook-items :page hook)
-                   for item-identifier = (item-identifier item)
-                   if (eql identifier item-identifier)
-                   return item))))
+        if (uri-matches uri request)
+          do (return (loop for item in (hook-items :page hook)
+                           if (eql identifier (item-identifier item))
+                             return item))))
 
 (define-interface-method dispatcher:register (hook identifier uri)
   (let ((found (find uri *hooks* :key #'third :test #'uri-same)))
     (when found
       (if (not (eql hook (first found)))
-          (v:warn :flash "Overriding existing trigger ~a on ~a" (car found) uri))
+          (v:warn :flash-dispatch "Overriding existing trigger ~a on ~a" (car found) uri))
       (setf *hooks* (delete uri *hooks* :key #'third :test #'uri-same))))
-  (v:debug :flash "Registering ~a for ~a/~a" uri identifier hook)
+  (v:debug :flash-dispatch "Registering ~a for ~a/~a" uri identifier hook)
   (setf *hooks* (sort (append *hooks* `((,hook ,identifier ,uri))) #'sort-dispatcher-hooks))
   hook)
 
