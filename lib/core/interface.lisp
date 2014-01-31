886792d846050389e4055bf05dca6d59d9cf71b7
Shinmera <shinmera@tymoon.eu>
2014-01-31 13:06:31 +0100

    Moved lambda-list manipulation functions to toolkit.

--- lib/core/interface.lisp
+++ lib/core/interface.lisp
@@ -6,57 +6,6 @@
 
 (in-package :radiance)
 
-(defun lambda-keyword-p (symbol)
-  (find symbol '(&allow-other-keys &aux &body &environment &key &optional &rest &whole)))
-
-(defun flatten-lambda-list (lambda-list)
-  (mapcar #'(lambda (a) (if (listp a) (car a) a)) lambda-list))
-
-(defun extract-lambda-vars (lambda-list)
-  (remove-if #'lambda-keyword-p (flatten-lambda-list lambda-list)))
-
-(defun extract-macro-lambda-vars (macro-lambda-list)
-  (loop with varlist = ()
-     for i from 0 below (length macro-lambda-list)
-     for arg in macro-lambda-list
-     do (when (find arg '(&key &optional &rest &body &aux))
-          (return (append varlist (extract-lambda-vars (nthcdr i macro-lambda-list)))))
-       (unless (lambda-keyword-p arg)
-         (if (listp arg)
-             (appendf varlist (extract-lambda-vars arg))
-             (appendf varlist (list arg))))
-     finally (return varlist)))
-
-(defun make-key-extensible (generic-lambda-list)
-  (if (or (find '&rest generic-lambda-list)
-          (find '&body generic-lambda-list))
-      generic-lambda-list
-      (if (find '&key generic-lambda-list)
-          (append generic-lambda-list '(&allow-other-keys))
-          (append generic-lambda-list '(&key &allow-other-keys)))))
-
-(defun macro-lambda-list->generic-list (macro-lambda-list)
-  (loop with in-required-args = T
-     for arg in macro-lambda-list
-     collect (cond
-               ((eql arg '&body)
-                (setf in-required-args NIL)
-                '&rest)
-               ((or (eql arg '&rest) (eql arg '&key) (eql arg '&optional))
-                (setf in-required-args NIL)
-                arg)
-               ((and in-required-args (listp arg))
-                (gensym (format NIL "~{~a~^-~}" (extract-lambda-vars arg))))
-               ((listp arg)
-                (car arg))
-               (T arg))))
-
-(defun intern-list-symbols (list package)
-  (loop for element in list
-     collect (if (and (symbolp element) (not (keywordp element)) (not (null element)))
-                 (intern (string-upcase element) package)
-                 element)))
-
 (defmacro define-interface-component-expander (name (name-var args-var options-var &optional (interface-var (gensym "INTERFACE-NAME"))) &body body)
   "Defines a new interface component expander.
 NAME         --- A symbol designating the component's type name.
--- lib/core/toolkit.lisp
+++ lib/core/toolkit.lisp
@@ -166,3 +166,64 @@ If if-does-not-exist is :error, an error is thrown in case the directory cannot 
              (walk dir))
             ((eq if-does-not-exist :error)
              (error "File ~S does not exist." dir))))))
+
+(defun find-any (items sequence &rest args &key from-end (start 0) end key test test-not)
+  (declare (ignore from-end start end key test test-not))
+  (not (loop for item in items
+             never (apply #'find item sequence args))))
+
+(defun lambda-keyword-p (symbol)
+  (find symbol '(&allow-other-keys &aux &body &environment &key &optional &rest &whole)))
+
+(defun remove-aux-part (lambda-list)
+  (if-let ((position (position '&aux lambda-list)))
+    (subseq lambda-list 0 position)
+    lambda-list))
+
+(defun flatten-lambda-list (lambda-list)
+  (mapcar #'(lambda (a) (if (listp a) (car a) a)) lambda-list))
+
+(defun extract-lambda-vars (lambda-list)
+  (remove-if #'lambda-keyword-p (flatten-lambda-list (remove-aux-part lambda-list))))
+
+(defun extract-macro-lambda-vars (macro-lambda-list)
+  (loop with varlist = ()
+        for i from 0 below (length macro-lambda-list)
+        for arg in macro-lambda-list
+        do (when (find arg '(&key &optional &rest &body &aux))
+             (return (append varlist (extract-lambda-vars (nthcdr i macro-lambda-list)))))
+           (unless (lambda-keyword-p arg)
+             (if (listp arg)
+                 (appendf varlist (extract-lambda-vars arg))
+                 (appendf varlist (list arg))))
+        finally (return varlist)))
+
+(defun make-key-extensible (generic-lambda-list)
+  (if (or (find '&rest generic-lambda-list)
+          (find '&body generic-lambda-list))
+      generic-lambda-list
+      (if (find '&key generic-lambda-list)
+          (append generic-lambda-list '(&allow-other-keys))
+          (append generic-lambda-list '(&key &allow-other-keys)))))
+
+(defun macro-lambda-list->generic-list (macro-lambda-list)
+  (loop with in-required-args = T
+        for arg in macro-lambda-list
+        collect (cond
+                  ((eql arg '&body)
+                   (setf in-required-args NIL)
+                   '&rest)
+                  ((or (eql arg '&rest) (eql arg '&key) (eql arg '&optional))
+                   (setf in-required-args NIL)
+                   arg)
+                  ((and in-required-args (listp arg))
+                   (gensym (format NIL "~{~a~^-~}" (extract-lambda-vars arg))))
+                  ((listp arg)
+                   (car arg))
+                  (T arg))))
+
+(defun intern-list-symbols (list package)
+  (loop for element in list
+        collect (if (and (symbolp element) (not (keywordp element)) (not (null element)))
+                    (intern (string-upcase element) package)
+                    element)))
