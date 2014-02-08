fb7ca1abde73844078d1dddcd7b53f601c0ad194
Shinmera <shinmera@tymoon.eu>
2014-02-08 13:33:59 +0100

    Fixed mechanism interfaces and functions

--- mod/verify/mechanism.lisp
+++ mod/verify/mechanism.lisp
@@ -14,7 +14,7 @@
   ((name :initarg :name :initform "UNNAMED" :accessor name :allocation :class)))
 
 (defun get-mechanisms ()
-  (mapcar #'make-instance (class-name (closer-mop:class-direct-subclasses (find-class 'mechanism)))))
+  (mapcar #'(lambda (a) (make-instance (class-name a))) (closer-mop:class-direct-subclasses (find-class 'mechanism))))
 
 (defgeneric page-login (mechanism)
   (:documentation "Called when the login page is built."))
@@ -23,10 +23,13 @@
   (:documentation "Called when the register page is built."))
 
 (defgeneric page-settings (mechanism)
-  (:documentation "Called when the user settings panel is built."))
+  (:documentation "Called when the admin settings panel is built."))
 
-(defgeneric linked-p (mechanism)
+(defgeneric linked-p (mechanism user)
   (:documentation "Called to check during the registration process whether the mechanism has been linked or not. Should return a generalized boolean."))
+
+(defgeneric finalize-registration (mechanism user)
+  (:documentation "Called to finalize the registration process and save data about a user."))
 
 (defmethod page-login ((mechanism mechanism))
   (warn 'mechanism-method-missing :method-name 'page-login))
@@ -37,8 +40,11 @@
 (defmethod page-settings ((mechanism mechanism))
   (warn 'mechanism-method-missing :method-name 'page-login))
 
-(defmethod linked-p ((mechanism mechanism))
+(defmethod linked-p ((mechanism mechanism) user)
   (warn 'mechanism-method-missing :method-name 'page-login))
+
+(defmethod finalize-registration ((mechanism mechanism) user)
+  (warn 'mechanism-method-missing :method-name 'finalize-registration))
 
 (define-interface-extension auth
   (error-out (process code text)
@@ -48,7 +54,10 @@
     (:documentation "Automatically catches any error of type RADIANCE-ERROR and executes the proper ERROR-OUT method."))
   (define-mechanism (name &body forms)
     (:type :macro)
-    (:documentation "Define a mechanism. Expected as body forms are (:LOGIN form*) (:REGISTER form*) (:SETTINGS form*) (:LINKED-P form*).")))
+    (:documentation "Define a mechanism. Expected as body forms are (:LOGIN form*) (:REGISTER form*) (:SETTINGS form*) (:LINKED-P form*) (:FINALIZE form*).")))
+
+(radiance::define-indentation 'define-mechanism '(4 &rest (&whole 2 &lambda 4 &body)))
+(radiance::initialize-slime)
 
 (define-interface-method auth:error-out ((process (eql :login)) code text)
   (server:redirect (format NIL "/login?errortext=~a&errorcode=~a" text code)))
@@ -68,20 +77,24 @@
   (let ((login (cdr (assoc :login forms)))
         (register (cdr (assoc :register forms)))
         (settings (cdr (assoc :settings forms)))
-        (linked-p (cdr (assoc :linked-p forms))))
+        (linked-p (cdr (assoc :linked-p forms)))
+        (finalize (cdr (assoc :finalize forms))))
     `(progn
        (defclass ,name (mechanism) ((name :initform ,(string name) :accessor name)))
        ,(when login
-          `(defmethod page-login ((,name ,name))
+          `(defmethod page-login ((,(caar login) ,name) ,@(cdar login))
              (auth:with-redirecting (:login)
-               ,@login)))
+               ,@(cdr login))))
        ,(when register
-          `(defmethod page-register ((,name ,name))
+          `(defmethod page-register ((,(caar register) ,name) ,@(cdar register))
              (auth:with-redirecting (:register)
-               ,@register)))
+               ,@(cdr register))))
        ,(when settings
-          `(defmethod page-settings ((,name ,name))
-             ,@settings))
+          `(defmethod page-settings ((,(caar settings) ,name) ,@(cdar settings))
+             ,@(cdr settings)))
        ,(when linked-p
-          `(defmethod linked-p ((,name ,name))
-             ,@linked-p)))))
+          `(defmethod linked-p ((,(caar linked-p) ,name) ,@(cdar linked-p))
+             ,@(cdr linked-p)))
+       ,(when finalize
+          `(defmethod finalize-registration ((,(caar finalize) ,name) ,@(cdar finalize))
+             ,@(cdr finalize))))))
