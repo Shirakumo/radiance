0344aa7cabeae663dba656eca624866091f4a745
Shinmera <shinmera@tymoon.eu>
2014-02-08 13:33:31 +0100

    Added sample radiance config.

--- lib/core/globals.lisp
+++ lib/core/globals.lisp
@@ -8,6 +8,8 @@
 
 (defvar *radiance-startup-time*          0  "Startup time for uptime and such.")
 (defvar *radiance-config-file*           NIL "Radiance's main JSON configuration file.")
+(defvar *radiance-config-sample-file*    (merge-pathnames "sample-radiance.json" (asdf:system-source-directory :radiance))
+                                         "Radiance's sample JSON configuration file.")
 (defvar *radiance-config*                NIL "Radiance's main static configuration.")
 (defvar *radiance-request*               NIL "Current request object.")
 (defvar *radiance-response*              NIL "Current reply object.")
--- lib/core/toolkit.lisp
+++ lib/core/toolkit.lisp
@@ -25,6 +25,9 @@
     (v:warn :radiance.server.status "Config-file or *radiance-config-file* are NIL, automatically choosing path through ASDF.")
     (setf config-file (merge-pathnames "radiance.json" (asdf:system-source-directory :radiance))))
 
+  (unless (probe-file *radiance-config-file*)
+    (v:warn :radiance.server.status "Configuration file ~a not found, loading from sample instead.")
+    (setf config-file *radiance-config-sample-file*))
   (v:debug :radiance.server.status "Reloading radiance config file from ~a" config-file)
   (with-open-file (file config-file :if-does-not-exist :ERROR)
     (setf *radiance-config* (json:decode-json file))
