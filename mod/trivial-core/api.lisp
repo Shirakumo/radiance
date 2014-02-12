ccf04fb2038225615ee66bbdc09bc6618a607033
Shinmera <shinmera@tymoon.eu>
2014-02-12 14:54:19 +0100

    Rewrote API system to be callable and introspect-able

--- mod/trivial-core/api.lisp
+++ mod/trivial-core/api.lisp
@@ -6,6 +6,29 @@
 
 (in-package :radiance)
 
+(defvar *apis* (make-hash-table :test #'equal))
+;; path => ((method function access)*)
+(defvar *formats* (make-hash-table) "Map of all API output formats.")
+
+(defun add-api-function (path method function access)
+  (setf path (string-downcase path))
+  (let ((methodlist (or (gethash path *apis*) (setf (gethash path *apis*) ()))))
+    (let ((position (position method methodlist :key #'first))
+          (method (list method function access)))
+      (if position
+          (progn
+            (v:debug :radiance.core.api "Redefining ~a ~a" path method)
+            (setf (nth position methodlist) method))
+          (progn
+            (v:debug :radiance.core.api "Defining new API method ~a ~a" path method)
+            (appendf (gethash path *apis*) (list method)))))))
+
+(defun find-api-method (path method)
+  (setf path (string-downcase path))
+  (let ((methodlist (gethash path *apis*)))
+    (or (find method methodlist :key #'car)
+        (find T methodlist :key #'car))))
+
 (define-interface-method core:define-api (name args options &body body)
   "Define a new api function.
 See interface definition for a description of the arguments.
@@ -22,37 +45,15 @@ request methods."
           () "Only &optional, &key and &aux operators are allowed here.")
   (destructuring-bind (&key (method T) access-branch (identifier `(context-module-identifier))) options
     (assert (find method '(T :GET :POST :PUT :PATCH :DELETE)) () "Method has to be one of T :GET :POST :PUT :PATCH :DELETE")
-    (let* ((argsgens (gensym "ARGUMENTS"))
-           (arggens (gensym "ARG"))
-           (identgens (gensym "IDENTIFIER"))
-           (prepared-args (cons '&key (remove-if #'(lambda (a) (find a '(&key &optional))) args)))
-           (documentation (if (stringp (car body)) (car body) NIL))
-           (methodfun (case method (:POST 'server:post) (:GET 'server:get) (otherwise 'server:post-or-get)))
-           (function `(apply #'(lambda ,prepared-args ,@body)
-                             (let ((,argsgens ()))
-                               ,@(mapcar #'(lambda (arg)
-                                             `(when-let (,arggens (,methodfun ,(string-downcase arg)))
-                                                (push ,arggens ,argsgens)
-                                                (push ,(make-keyword arg) ,argsgens)))
-                                         (extract-lambda-vars prepared-args))
-                               ,argsgens))))
-      `(let ((,identgens ,identifier))
-         (v:debug :radiance.server.site "Defining API page ~a for ~a" ',name ,identgens)
-         (define-hook (:api (make-keyword (format NIL "~a/~a" ,identgens ',name))) (:identifier (make-keyword (format NIL "~a:~a" ,identgens ,method)) :documentation ,documentation)
-           ,@(loop for arg in args until (lambda-keyword-p arg)
-                   collect `(assert (not (null (,methodfun ,(string-downcase arg))))
-                                    () 'api-args-error :module ,identgens :apicall ',name :text (format NIL "Argument ~a required." ',arg)))
-           ,(when access-branch
-              `(progn (ignore-errors (auth:authenticate))
-                      (assert (user:check ,access-branch)
-                              () 'api-auth-error :module ,identgens :apicall ',name :text "Not authorized.")))
-           ,function)))))
+    (when-let ((position (position '&optional args)))
+      (setf (nth position args) '&key))
+    `(add-api-function (format NIL "~a/~a" ,identifier ',name) ,method #'(lambda ,args ,@body) ,access-branch)))
 
 (define-interface-method core:api-return (code text &key data)
   (plist->hash-table :CODE code :TEXT text :TIME (get-unix-time) :DATA data))
 
 (define-interface-method core:api-format (format data)
-  (let ((format (gethash format *radiance-api-formats*)))
+  (let ((format (gethash format *formats*)))
     (if format
         (progn
           (server:set-content-type (second format))
@@ -63,18 +64,19 @@ request methods."
   (let ((name (make-keyword name)))
     `(progn
        (v:debug :radiance.server.site "Defining new api format ~a, ~a" ,name ,content-type)
-       (setf (gethash ,name *radiance-api-formats*)
+       (setf (gethash ,name *formats*)
              (list ,name ,content-type
                    (lambda (,datavar) ,@body))))))
 
-;;; REFACTOR ALL THIS SHIT
-(define-interface-method core:api-call (apicall method &key post get)
-  
-  (let* ((pathparts (split-sequence:split-sequence #\/ (path *radiance-request*)))
-         (module (cadr pathparts))
-         (trigger (make-keyword (string-upcase (concatenate-strings (cdr pathparts) "/"))))
-         (hooks (hook-items :api trigger)))
-    (or (call-api module hooks)
+
+(define-interface-method core:api-call (apicall method &rest vars)
+  (let ((call (find-api-method apicall method)))
+    (unless call
+      (error 'api-no-call-error :apicall apicall :text "No matching apicall found!"))
+    (when (third call)
+      (unless (user:check (third call) :user (user:current :authenticate T))
+        (error 'api-auth-error :apicall apicall :text "Not authorized to execute this api call.")))
+    (or (apply (second call) vars)
         (core:api-return 204 "No return data"))))
 
 (core::m-define-api-format :trivial-core none "text/plain; charset=utf-8" data
@@ -87,56 +89,55 @@ request methods."
 (core::m-define-api-format :trivial-core plist "text/x-lisp-plist" data
   (write-to-string data))
 
+(defun generate-api-arguments-list (path lambda-list method)
+  (loop with retrieval-method = (case method
+                                  (:GET #'server::i-get)
+                                  (:POST #'server::i-post)
+                                  (otherwise #'server::i-post-or-get))
+        with arguments = ()
+        with in-keywords = NIL
+        for arg in lambda-list
+        do (if (eql arg '&key)
+               (setf in-keywords T)
+               (let ((val (funcall retrieval-method server:*implementation* (string-downcase arg))))
+                 (if in-keywords
+                     (when val
+                       (push (find-symbol (string arg) :KEYWORD) arguments)
+                       (push val arguments))
+                     (if val
+                         (push val arguments)
+                         (error 'api-args-error :apicall path :text (format NIL "Argument ~a required, but not set." arg))))))
+        finally (return (nreverse arguments))))
+
 (core::m-define-page :trivial-core api #u"/api/" ()
   (server:set-content-type "none")
-  (let* ((pathparts (split-sequence:split-sequence #\/ (path *radiance-request*)))
+  (let* ((pathparts (split-sequence:split-sequence #\/ (path *radiance-request*) :remove-empty-subseqs T))
+         (path (concatenate-strings (cdr pathparts) "/"))
          (format (make-keyword (string-upcase (or (server:get "format") (server:post "format") "json"))))
-         (value  (handler-case 
-                     (case (length pathparts)
-                       ((1 2) (core:api-return 200 (format NIL "Radiance API v~a" (asdf:component-version (context-module)))
-                                               :data (plist->hash-table :VERSION (asdf:component-version (context-module)))))
-                       (otherwise
-                        (let* ((module (cadr pathparts))
-                               (trigger (make-keyword (string-upcase (concatenate-strings (cdr pathparts) "/"))))
-                               (hooks (hook-items :api trigger)))
-                          (or (call-api module hooks)
-                              (core:api-return 204 "No return data")))))
-                   (api-args-error (c)
-                     (core:api-return 400 "Invalid arguments"
-                                      :data (plist->hash-table :errortype (class-name (class-of c)) 
-                                                               :code (slot-value c 'code)
-                                                               :text (slot-value c 'text))))
-                   (api-error (c)
-                     (core:api-return 500 "Api error"
-                                      :data (plist->hash-table :errortype (class-name (class-of c))
-                                                               :code (slot-value c 'code)
-                                                               :text (slot-value c 'text)))))))
+         (value (handler-case 
+                    (or (when (< (length pathparts) 2)
+                          (core:api-return 200 (format NIL "Radiance API v~a" (asdf:component-version (context-module)))
+                                           :data (plist->hash-table :VERSION (asdf:component-version (context-module)))))
+                        (when-let ((call (find-api-method path (server:request-method))))
+                          (or
+                           (when (third call)
+                             (unless (user:check (third call) :user (user:current :authenticate T))
+                               (core:api-return 403 "Not authorized")))
+                           (let* ((lambda-list (flatten-lambda-list (sb-introspect:function-lambda-list (second call))))
+                                  (arguments (generate-api-arguments-list path lambda-list (server:request-method))))
+                             (apply (second call) arguments))
+                           (core:api-return 204 "No return data")))
+                        (core:api-return 404 "Call not found"))
+                  (api-args-error (c)
+                    (core:api-return 400 "Invalid arguments"
+                                     :data (plist->hash-table :errortype (class-name (class-of c)) 
+                                                              :code (slot-value c 'code)
+                                                              :text (slot-value c 'text))))
+                  (api-error (c)
+                    (core:api-return 500 "Api error"
+                                     :data (plist->hash-table :errortype (class-name (class-of c))
+                                                              :code (slot-value c 'code)
+                                                              :text (slot-value c 'text)))))))
     (if (string-equal (server:content-type) "none")
         (core:api-format format value)
         value)))
-
-(defun identifier-and-method (item-identifier)
-  (let* ((item-identifier (string item-identifier))
-         (colonpos (position #\: item-identifier)))
-    (if colonpos
-        (values (subseq item-identifier 0 colonpos)
-                (subseq item-identifier (1+ colonpos)))
-        (values item-identifier NIL))))
-
-(defun identifier-matches-p (item-identifier identifier &optional (method (server:request-method)))
-  (multiple-value-bind (item-identifier item-method) (identifier-and-method item-identifier)
-    (and (string-equal item-identifier identifier)
-         (or (not item-method)
-             (string-equal item-method "T")
-             (string-equal item-method (string method))))))
-
-(defun call-api (module hook-items)
-  (loop with return = ()
-     with accepted = NIL
-     for item in hook-items
-     if (identifier-matches-p (string (item-identifier item)) module)
-     do (setf accepted T)
-       (appendf return (funcall (item-function item)))
-     finally (return (if accepted
-                         return
-                         (core:api-return 404 "Call not found")))))
