10a4e5ef16e00623cf20f6957900cb7fbc7e7d2a
Shinmera <shinmera@tymoon.eu>
2014-07-27 13:26:04 +0200

    Added config stuff

--- toolkit.lisp
+++ toolkit.lisp
@@ -5,3 +5,21 @@
 |#
 
 (in-package #:org.tymoonnext.radiance.lib.radiance.core)
+
+(defvar *config* NIL)
+(defvar *root* (asdf:system-source-directory :radiance))
+(defvar *config-path* (merge-pathnames (make-pathname :name "radiance.uc" :type "lisp") *root*))
+(defvar *data-path* (merge-pathnames (make-pathname :directory '(:relative "data")) *root*))
+
+(defun load-config (&optional (path *config-path*))
+  (setf *config* (uc:load-configuration path :format :lisp)
+        *config-path* path)
+  T)
+
+(defun config-tree (&rest branches)
+  (let ((uc:*config* *config*))
+    (apply #'uc:config-tree branches)))
+
+(defun (setf config-tree) (value &rest branches)
+  (let ((uc:*config* *config*))
+    (apply #'(setf uc:config-tree) value branches)))
