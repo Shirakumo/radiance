6c39715fcf7c4ab62cf0c6416d1965bfc17397e1
Shinmera <shinmera@tymoon.eu>
2014-07-27 18:22:56 +0200

    Imported a bunch from past

--- toolkit.lisp
+++ toolkit.lisp
@@ -28,3 +28,46 @@
 (defun (setf config-tree) (value &rest branches)
   (let ((uc:*config* *config*))
     (apply #'(setf uc:config-tree) value branches)))
+
+(defun make-keyword (name)
+  (let ((name (string name)))
+    (or (find-symbol name "KEYWORD")
+        (intern name "KEYWORD"))))
+
+(declaim (inline concatenate-strings))
+(defun concatenate-strings (list &optional (delim ""))
+  "Joins a list of strings into one string using format."
+  (format nil (format nil "~~{~~A~~^~a~~}" delim) list))
+
+(declaim (inline universal-to-unix-time))
+(defun universal-to-unix-time (universal-time)
+  (- universal-time +unix-epoch-difference+))
+
+(declaim (inline unix-to-universal-time))
+(defun unix-to-universal-time (unix-time)
+  (+ unix-time +unix-epoch-difference+))
+
+(declaim (inline get-unix-time))
+(defun get-unix-time ()
+  "Returns a unix timestamp."
+  (universal-to-unix-time (get-universal-time)))
+
+(defun make-random-string (&optional (length 16) (chars *random-string-characters*))
+  "Generates a random string of alphanumerics."
+  (coerce (loop with charlength = (length chars) 
+                for i below length collect (aref chars (random charlength)))
+          'string))
+
+(defun file-size (pathspec)
+  "Retrieves the file size in bytes."
+  (with-open-file (in pathspec :direction :input :element-type '(unsigned-byte 8))
+    (file-length in)))
+
+(defun read-data-file (pathspec &key (if-does-not-exist :ERROR))
+  "Returns the file contents in string format. Any path is relative to the radiance data directory."
+  (with-open-file (stream (merge-pathnames pathspec *data-path*) :if-does-not-exist if-does-not-exist)
+    (with-output-to-string (string)
+      (let ((buffer (make-array 4096 :element-type 'character)))
+        (loop for bytes = (read-sequence buffer stream)
+              do (write-sequence buffer string :start 0 :end bytes)
+              while (= bytes 4096))))))
