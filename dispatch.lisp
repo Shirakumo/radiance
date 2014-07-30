905fabf23325d7f02cd2b41cab8a7c15614d8d16
Shinmera <shinmera@tymoon.eu>
2014-07-30 10:21:56 +0200

    Various bugfixes, moved request/response classdefs away

--- dispatch.lisp
+++ dispatch.lisp
@@ -6,24 +6,6 @@
 
 (in-package #:org.tymoonnext.radiance.lib.radiance.web)
 
-(defclass request (uri)
-  ((headers :initarg :headers :initform () :accessor headers)
-   (http-method :initarg :http-method :initform "GET" :accessor http-method)
-   (post-data :initarg :post-data :initform () :accessor post-data)
-   (get-data :initarg :get-data :initform () :accessor get-data)
-   (cookies :initarg :cookies :initform () :accessor cookies)
-   (remote :initarg :remote :initform "unknown" :accessor remote)))
-
-(defclass response ()
-  ((data :initarg :data :initform NIL :accessor data)
-   (headers :initarg :headers :initform () :accessor headers)
-   (content-type :initarg :content-type :initform "text/plain" :accessor content-type)
-   (return-code :initarg :return-code :initform 200 :accessor return-code)
-   (external-format :initarg :external-format :initform :UTF-8 :accessor external-format)))
-
-(defmethod print-object ((response response) stream)
-  (format stream "~s" (data response)))
-
 (defvar *uri-registry* (make-hash-table :test 'eql))
 (defvar *uri-priority* (make-array 0))
 
@@ -36,10 +18,11 @@
 (defun (setf uri-dispatcher) (uri-or-f name &optional uri)
   (if uri
       (setf uri-or-f (make-uri-dispatcher uri uri-or-f))
-      (unless (typep uri-or-f 'uri-registry)
-        (error 'type-error :datum uri-or-f :expected-type 'uri-registry)))
+      (unless (typep uri-or-f 'uri-dispatcher)
+        (error 'type-error :datum uri-or-f :expected-type 'uri-dispatcher)))
   (setf (gethash name *uri-registry*) uri-or-f)
-  (rebuild-uri-priority))
+  (rebuild-uri-priority)
+  uri-or-f)
 
 (defun make-uri-dispatcher (uri dispatch-function)
   (let ((uri (copy-uri uri)))
@@ -49,15 +32,19 @@
 
 (defun rebuild-uri-priority ()
   (setf *uri-priority*
-        (sort (loop with array = (make-array (hash-table-count *uri-regex*))
+        (sort (loop with array = (make-array (hash-table-count *uri-registry*))
                     for uri being the hash-values of *uri-registry*
-                    for i from 0 do (setf (aref array i) uri)) #'uri>)))
+                    for i from 0 do (setf (aref array i) uri)
+                    finally (return array)) #'uri>)))
 
 (defmacro define-uri-dispatcher (name (uri &optional (requestvar 'request)) &body body)
   `(eval-when (:compile-toplevel :load-toplevel :execute)
-     (setf (uri-dispatcher ',name ,uri) #'(lambda (,requestvar) ,@body))))
+     (setf (uri-dispatcher ',name ,uri)
+           #'(lambda (,requestvar)
+               (declare (ignorable ,requestvar))
+               ,@body))))
 
-(defun dispatch (request)
+(defun dispatch (uri-data-object)
   (loop for uri across *uri-priority*
-        when (uri-matches request uri)
-          do (return (funcall (dispatch-function uri) request))))
+        when (uri-matches uri-data-object uri)
+          do (return (funcall (dispatch-function uri) uri-data-object))))
