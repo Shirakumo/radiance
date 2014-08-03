2c1a036ed2ae3169408c808c68c3e2f5b15b570f
Shinmera <shinmera@tymoon.eu>
2014-08-03 15:56:04 +0200

    API prototype

---
+++ api.lisp
@@ -0,0 +1,105 @@
+#|
+ This file is a part of Radiance
+ (c) 2014 TymoonNET/NexT http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:org.tymoonnext.radiance.lib.radiance.web)
+
+(defvar *api-pages* (make-hash-table :test 'equalp))
+
+(defun api-page (path)
+  (gethash path *api-pages*))
+
+(defun (setf api-page) (page path)
+  (setf (gethash path *api-pages*) page))
+
+(defclass api-page ()
+  ((handler :initarg :handler :initform (error "HANDLER function required.") :accessor handler)
+   (argslist :initarg :argslist :initform () :accessor argslist)
+   (docstring :initarg :docstring :initform NIL :accessor docstring)))
+
+(defun api-call (api-page request)
+  (loop with args = ()
+        with in-optional = NIL
+        for arg in (argslist api-page)
+        do (cond
+             ((eql arg '&optional)
+              (setf in-optional T))
+             ((and in-optional (listp arg))
+              (let ((val (server:post-or-get (string (first arg)) request)))
+                (push (or val (second arg)) args)))
+             (in-optional
+              (push (server:post-or-get (string arg) request) args))
+             (T
+              (let ((val (server:post-or-get (string arg) request)))
+                (if val (push val args) (error 'api-argument-missing :argument arg)))))
+        finally (apply (handler api-page) (nreverse args))))
+
+(defvar *api-formats* (make-hash-table :test 'equalp))
+(defvar *default-api-format* "lisp")
+
+(defun api-format (name)
+  (gethash name *api-formats*))
+
+(defun (setf api-format) (parse-func name)
+  (setf (gethash name *api-formats*) parse-func))
+
+(defmacro define-api-format (name (argsvar) &body body)
+  `(setf (api-format ,(string name))
+         #'(lambda (,argsvar) ,@body)))
+
+(defun api-convert (data)
+  (unless data (error 'api-response-empty))
+  (let ((format (or (server:get "format") *default-api-format*)))
+    (funcall (or (api-format format)
+                 (error 'api-unknown-format :format format))
+             data)))
+
+(defgeneric api-serialize (object))
+
+(defvar *api-options* (make-hash-table))
+
+(defun api-option (name)
+  (gethash name *api-options*))
+
+(defun (setf api-option) (option name)
+  (setf (gethash name *api-options*) option))
+
+(defmacro define-api-option (name (namevar argsvar &rest rest) &body body)
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     (setf (api-option ,(make-keyword name))
+           #'(lambda (,namevar ,argsvar ,@rest)
+               ,@body))))
+
+(defvar *api-body*)
+(defmacro define-api (name args options &body body)
+  (let ((*api-body* body)
+        (no-value (gensym "NO-VALUE")))
+    `(eval-when (:compile-toplevel :load-toplevel :execute)
+       ,@(loop for option being the hash-keys of *api-options*
+               for function being the hash-values of *api-options*
+               for value = (getf options option no-value)
+               for result = (if (eql value no-value)
+                                (funcall function name args)
+                                (funcall function name args value))
+               when result
+                 collect result)
+       (setf (api-page ,(string name))
+             (make-instance
+              'api-page
+              :argslist ',args
+              :handler #'(lambda ,(extract-lambda-vars args) ,@*api-body*)
+              :docstring ,(getf options :documentation))))))
+
+
+(define-page api #u"/api/.*" ()
+  (let* ((slashpos (position #\/ (path *request*)))
+         (subpath (subseq (path *request*) (1+ slashpos)))
+         (api-page (or (api-page subpath) (api-page ""))))
+    (handler-case
+        (api-convert (api-call api-page *request*))
+      (api-error (err)
+        (api-convert err))
+      (error (err)
+        ))))
