2adff9143f3ac747eea47e863669b3617a3a9a53
Shinmera <shinmera@tymoon.eu>
2014-08-03 14:55:46 +0200

    Toolkit funcs

---
+++ toolkit.lisp
@@ -0,0 +1,26 @@
+#|
+ This file is a part of Radiance
+ (c) 2014 TymoonNET/NexT http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:org.tymoonnext.radiance.lib.radiance.web)
+
+(defun lambda-keyword-p (symbol)
+  "Returns the symbol if it is a lambda-keyword symbol (the &-options)."
+  (find symbol '(&allow-other-keys &aux &body &environment &key &optional &rest &whole)))
+
+(defun remove-aux-part (lambda-list)
+  "Removes the &aux part of the lambda-list."
+  (let ((position (position '&aux lambda-list)))
+    (if position
+        (subseq lambda-list 0 position)
+        lambda-list)))
+
+(defun flatten-lambda-list (lambda-list)
+  "Flattens the lambda-list by replacing all lists within it with their respective first symbol."
+  (mapcar #'(lambda (a) (if (listp a) (car a) a)) lambda-list))
+
+(defun extract-lambda-vars (lambda-list)
+  "Extracts the symbols that name the variables in the lambda-list."
+  (remove-if #'lambda-keyword-p (flatten-lambda-list (remove-aux-part lambda-list))))
