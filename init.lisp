6c8b2d0a73595b775ddd8b826fe02eeeea3a4767
Shinmera <shinmera@tymoon.eu>
2014-08-07 23:47:16 +0200

    Removed DB hooks

--- init.lisp
+++ init.lisp
@@ -6,8 +6,6 @@
 
 (in-package #:org.tymoonnext.radiance.lib.radiance.web)
 
-(define-hook db-ready ())
-(define-hook db-shutdown ())
 (define-hook server-ready ())
 (define-hook server-shutdown ())
 (define-hook startup-done ())
@@ -20,11 +18,6 @@
   (l:info :radiance "Ensuring prerequisites.")
   (unless (implementation 'server)
     (load-implementation 'server))
-  
-  (when (implementation 'db)
-    (l:info :radiance "Connecting to database.")
-    (db:connect (config-tree :database :name))
-    (trigger 'db-ready))
 
   (l:info :radiance "Starting server.")
   ;; (server:start)
@@ -35,11 +28,6 @@
   T)
 
 (define-trigger shutdown ()
-  (when (implementation 'db)
-    (l:info :radiance "Disconnecting from database.")
-    (trigger 'db-shutdown)
-    (db:disconnect))
-
   (l:info :radiance "Stopping server.")
   ;; (server:stop)
   (trigger 'server-shutdown)
