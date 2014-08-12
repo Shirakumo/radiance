dc0fe2c532432a865ffbc3d715712cd55ab516fe
Shinmera <shinmera@tymoon.eu>
2014-08-12 11:34:15 +0200

    Fixes

--- mod/r-simple-sessions/session.lisp
+++ mod/r-simple-sessions/session.lisp
@@ -19,7 +19,7 @@
    (fields :initarg :fields :initform (make-hash-table :test 'eql) :accessor fields)
    (timeout :initarg :timeout :initform (+ (get-universal-time) session:*default-timeout*) :accessor timeout)))
 
-(defmethod print-object ((session session) stream)
+(defmethod print-object ((session session:session) stream)
   (print-unreadable-object (session stream :type T)
     (format stream "~a " (id session))
     (local-time:format-timestring stream (local-time:universal-to-timestamp (timeout session)) :format *session-timeout-format*)))
@@ -27,9 +27,10 @@
 (defun make-cookie-value (session)
   (cryptos:encrypt (format NIL "~a-~a" (id session) (make-random-string (+ 4 (random 9)))) *session-key*))
 
-(defmethod initialize-instance :after ((session session) &key)
+(defmethod initialize-instance :after ((session session:session) &key)
   (l:debug :session "Starting session ~a" session)
   (setf (gethash (id session) *session-table*) session)
+  (trigger 'session:create session)
   (when (and (boundp '*request*) (boundp '*response*))
     (setf (cookie "radiance-session" :domain (domain *request*) :path "/" :timeout (timeout session) :http-only T)
           ;; Note: Add support for the secure flag through https options in the main framework
@@ -47,7 +48,7 @@
 (defun session:start ()
   (let ((cookie (cookie "radiance-session")))
     (or (and cookie (decode-session cookie))
-        (make-instance 'session))))
+        (make-instance 'session:session))))
 
 (defun session:get (session-id)
   (let ((session (gethash session-id *session-table*)))
