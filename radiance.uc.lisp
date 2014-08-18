fdd7c0bccdca94a74918f604efd2c03134b42618
Shinmera <shinmera@tymoon.eu>
2014-08-18 17:50:16 +0200

    Sample config

---
+++ radiance.uc.lisp
@@ -0,0 +1,33 @@
+
+{EQL
+ :INTERFACES {EQL
+              :DATABASE "-i-postmodern",
+              :DATA-MODEL "-r-simple-model",
+              :LOGGER "-i-verbose",
+              :SERVER "-i-hunchentoot",
+              :SESSION "-r-simple-sessions",
+              :USER "-r-simple-users",
+              :AUTH "-r-simple-auth",
+              :CACHE "-r-simple-cache",
+              :PROFILE "-r-simple-profile"},
+ :HUNCHENTOOT {EQL
+               :INSTANCES ({EQL
+                            :PORT 8080}
+                           {EQL
+                            :PORT 4545}),
+               :DOMAINS ("-localhost" "-linuz.com")},
+ :POSTMODERN {EQL
+              :CONNECTIONS {EQL
+                            :LOCALHOST {EQL
+                                        :HOST "-localhost",
+                                        :PORT 5432,
+                                        :USER "-radiance",
+                                        :PASS "-q#vRsABnFrJ417$!",
+                                        :DATABASE "-radiance"},
+                            :TUNNEL {EQL
+                                     :HOST "-127.0.0.1",
+                                     :PORT 5432,
+                                     :USER "-radiance",
+                                     :PASS "-q#vRsABnFrJ417$!",
+                                     :DATABASE "-radiance"}},
+              :DEFAULT :TUNNEL}} 
