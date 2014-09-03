ec509c6edde3f8c4085a16b04c66653ee6fd69ba
Shinmera <shinmera@tymoon.eu>
2014-09-03 13:01:36 +0200

    Fixed simple-auth for Clip0.5.2

--- drivers/r-simple-auth/auth.lisp
+++ drivers/r-simple-auth/auth.lisp
@@ -32,7 +32,7 @@
         (cryptos:pbkdf2-hash password *salt*))
   (user:save user))
 
-(define-page login #@"auth/^login" (:lquery (template "login.html"))
+(define-page login #@"auth/^login" (:lquery (template "login.ctml"))
   (r-clip:process (lquery:$ (node)))
   (when (get-var "msg")
     (lquery:$ "#msg" (text (get-var "msg"))))
