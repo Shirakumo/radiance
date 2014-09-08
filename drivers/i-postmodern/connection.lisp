7fe32f5f760de1c37b7fb10ed4965892961238f5
Shinmera <shinmera@tymoon.eu>
2014-09-08 17:23:40 +0200

    Added experimental automatic reconnection, skip restart on connect

--- drivers/i-postmodern/connection.lisp
+++ drivers/i-postmodern/connection.lisp
@@ -46,32 +46,35 @@
 (defmacro with-connection (&body body)
   `(let* ((*current-con* (acquire-connection))
           (postmodern:*database* *current-con*))
+     (unless (postmodern:connected-p *current-con*)
+       (postmodern:reconnect *current-con*))
      (unwind-protect
           (progn
             ,@body)
        (release-connection *current-con*))))
 
 (defun db:connect (database-name)
-  (flet ((err (msg) (error 'database-connection-failed :database database-name :message msg)))
-    (let ((conn (config-tree :postmodern :connections database-name)))
-      (unless conn (err "No such connection found."))
-      (when *current-db*
-        (warn 'database-connection-already-open :database database-name)
-        (db:disconnect))
-      ;; Spec restarts for already open.
-      (let ((host (or (gethash :host conn) "localhost"))
-            (port (or (gethash :port conn) 5432))
-            (user (gethash :user conn))
-            (pass (gethash :pass conn))
-            (db (or (gethash :database conn) (err "No database configured!"))))
-        (l:info :database "Connecting ~a ~a~:[~;:*~]@~a:~a/~a"
-                database-name user pass host port db)
-        (setf *current-setting* (list host port user pass db)
-              *current-db* database-name)
-        (dotimes (i db::*default-pool-size*)
-          (sleep 0.5)
-          (db::spawn-standard-connection))
-        (trigger 'db:connected)))))
+  (with-simple-restart (skip "Skip connecting.")
+    (flet ((err (msg) (error 'database-connection-failed :database database-name :message msg)))
+      (let ((conn (config-tree :postmodern :connections database-name)))
+        (unless conn (err "No such connection found."))
+        (when *current-db*
+          (warn 'database-connection-already-open :database database-name)
+          (db:disconnect))
+        ;; Spec restarts for already open.
+        (let ((host (or (gethash :host conn) "localhost"))
+              (port (or (gethash :port conn) 5432))
+              (user (gethash :user conn))
+              (pass (gethash :pass conn))
+              (db (or (gethash :database conn) (err "No database configured!"))))
+          (l:info :database "Connecting ~a ~a~:[~;:*~]@~a:~a/~a"
+                  database-name user pass host port db)
+          (setf *current-setting* (list host port user pass db)
+                *current-db* database-name)
+          (dotimes (i db::*default-pool-size*)
+            (sleep 0.5)
+            (db::spawn-standard-connection))
+          (trigger 'db:connected))))))
 
 (defun db:disconnect ()
   (l:info :database "Disconnecting ~a" *current-db*)
