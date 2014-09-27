148998c7d294aabb941b38a36767d8c0a108b10a
Shinmera <shinmera@tymoon.eu>
2014-09-27 10:42:02 +0200

    Fixed return value

--- drivers/r-simple-auth/auth.lisp
+++ drivers/r-simple-auth/auth.lisp
@@ -44,7 +44,7 @@
            (cond
              (redirect
               (redirect (format NIL "~a?msg=~a" (referer *request*) message))
-              (return-from simple-auth/login))
+              (return-from simple-auth/login ""))
              (T (error message)))))
     (when (auth:current)
       (err "Already logged in."))
