df68cbed619a708cfe4e5e279001f1c63ba7d0db
Shinmera <shinmera@tymoon.eu>
2014-10-19 14:42:19 +0200

    Merging INIT into CORE

--- merge-dir/init.lisp
+++ merge-dir/init.lisp
@@ -7,23 +7,63 @@
 (in-package #:org.tymoonnext.radiance.lib.radiance.core)
 
 (defvar *startup-time* NIL)
+(defvar *running* NIL)
 
+(define-hook server-start ())
+(define-hook server-ready ())
+(define-hook server-stop ())
+(define-hook server-shutdown ())
 (define-hook startup ())
+(define-hook startup-done ())
 (define-hook shutdown ())
+(define-hook shutdown-done ())
 
 (defun startup ()
+  (when *running*
+    (error "Radiance is already running!"))
+  
   (setf *startup-time* (get-universal-time))
   (load-config)
   (loop for module in (config-tree :startup)
         do (if (member :quicklisp *features*)
                (funcall (symbol-function (find-symbol "QUICKLOAD" :ql)) module)
                (asdf:load-system module)))
-  (trigger 'startup))
 
-(defun shutdown ()
+  (trigger 'startup)
+  
+  (unless (implementation 'logger)
+    (load-implementation 'logger))
+  (l:info :radiance "Starting up.")
+  (l:info :radiance "Ensuring prerequisites.")
+  (unless (implementation 'server)
+    (load-implementation 'server))
+
+  (l:info :radiance "Starting server.")
+  (trigger 'server-start)
+  (setf *running* T)
+  (trigger 'server-ready)
+
+  (trigger 'startup-done)
+  (l:info :radiance "Startup done.")
+  T)
+
+(defun shutdown ()  
+  (unless *running*
+    (error "Radiance is not running!"))
+  
   (trigger 'shutdown)
+  
+  (l:info :radiance "Stopping server.")
+  (trigger 'server-stop)
+  (setf *running* NIL)
+  (trigger 'server-shutdown)
+  
   (save-config)
-  (setf *startup-time* NIL))
+  (setf *startup-time* NIL)
+
+  (trigger 'shutdown-done)
+  (l:info :radiance "Shutdown done.")
+  T)
 
 (defun uptime ()
   (- (get-universal-time) *startup-time*))
