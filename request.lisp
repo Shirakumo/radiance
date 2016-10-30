3aa1840da21318e0c04bffaf0dfa24febde12feb
Shinmera <shinmera@tymoon.eu>
2016-10-30 13:48:41 +0100

    Trace when getting a request.

--- request.lisp
+++ request.lisp
@@ -196,6 +196,7 @@
                     :get-data (ensure-request-hash-table get)
                     :cookies (ensure-request-hash-table cookies)
                     :remote remote)))
+    (v:trace :core.request "Received request ~a" *request*)
     (setf (uri *request*) (represent-uri (uri *request*) representation))
     (execute-request
      *request*
