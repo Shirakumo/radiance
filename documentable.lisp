2ae248d98d8188fb3a650133bc45aee9fa1f3e3c
Shinmera <shinmera@tymoon.eu>
2016-11-03 13:00:35 +0100

    Fix documentable documentation default methods

--- documentable.lisp
+++ documentable.lisp
@@ -24,9 +24,9 @@
          ,direct-slots
          ,@(remove :find-function options :key #'first))
        ,@(when find-function
-           `((defmethod documentation ((name symbol) (type (eql ',name)))
+           `((defmethod documentation (name (type (eql ',name)))
                (documentation (,find-function name) T))
-             (defmethod (setf documentation) (string (name symbol) (type (eql ',name)))
+             (defmethod (setf documentation) (string name (type (eql ',name)))
                (setf (documentation (,find-function name) T) string))
              (docs:define-documentation-test ,name (name)
                (,find-function name)))))))
