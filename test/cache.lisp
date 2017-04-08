700de0435e860291ddc63cf279d888dc168734b0
Joram Schrijver <i@joram.io>
2017-04-08 16:52:38 +0200

    Add some tests for the cache interface

---
+++ test/cache.lisp
@@ -0,0 +1,23 @@
+#|
+This file is a part of Radiance
+(c) 2017 Shirakumo https://shirakumo.org/ (i@joram.io)
+Author: Joram Schrijver <i@joram.io>
+|#
+
+(in-package #:org.shirakumo.radiance.test)
+
+(define-test cache
+  :parent interfaces
+  (let ((cached-value (make-array 1 :element-type '(unsigned-byte 8)
+                                    :initial-contents '(42)))
+        (uncached-value (make-array 1 :element-type '(unsigned-byte 8)
+                                      :initial-contents '(43))))
+    (cache:renew 'cache-name-1)
+    (is eq nil (cache:get 'cache-name-1))
+    (cache:with-cache 'cache-name-1 nil
+      cached-value)
+    (cache:with-cache 'cache-name-1 nil
+      uncached-value)
+    (is equalp cached-value (cache:get 'cache-name-1))
+    (cache:renew 'cache-name-1)
+    (is eq nil (cache:get 'cache-name-1))))
