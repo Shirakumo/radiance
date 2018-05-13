ae79b98ca2c298361a6917e37e79d57d7089ad0b
Shinmera <shinmera@tymoon.eu>
2018-05-13 12:29:08 +0200

    Fix access checking for NIL

--- defaults.lisp
+++ defaults.lisp
@@ -28,8 +28,9 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defun transform-access-body (body branch type)
     (if (not (eql branch T))
-        `((unless (and (auth:current) (user:check (auth:current) ,branch))
-            (error ',type :message (format NIL "~a does not have access to ~s"
+        `((unless (and (auth:current)
+                       ,@(when branch `((user:check (auth:current) ,branch))))
+            (error ',type :message (format NIL "~a does not have access~@[ to ~s~]"
                                            (user:username (or (auth:current) (user:get "anonymous"))) ,branch)))
           ,@body)
         body)))
