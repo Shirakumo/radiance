dc272e77b48b8dcc83a5d8bf165303139e99c723
Shinmera <shinmera@tymoon.eu>
2018-08-04 17:45:25 +0200

    Minor

--- migrate.lisp
+++ migrate.lisp
@@ -184,7 +184,10 @@
 (define-version-migration (:radiance-core NIL :2.0.0)
   (let ((previous-config-directory (merge-pathnames "radiance/" (ubiquitous:config-directory))))
     (when (uiop:directory-exists-p previous-config-directory)
+      (l:info :radiance.migrate "Migrating previous configuration from ~a."
+              previous-config-directory)
       (loop for original-path in (uiop:subdirectories previous-config-directory)
             for environment = (car (last (pathname-directory environment)))
             for new-path = (environment-directory environment :configuration)
-            do (rename-file original-path new-path)))))
+            do (rename-file original-path new-path))
+      (uiop:delete-empty-directory previous-config-directory))))
