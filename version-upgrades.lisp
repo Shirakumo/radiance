5cc61a2e614478c6318c8a5d77caa2087367ab99
Shinmera <shinmera@tymoon.eu>
2018-08-08 10:44:46 +0200

    Minor fixes to make things actually somewhat work.

--- version-upgrades.lisp
+++ version-upgrades.lisp
@@ -9,10 +9,15 @@
 (define-version-migration radiance-core (NIL 2.0.0)
   (let ((previous-config-directory (merge-pathnames "radiance/" (ubiquitous:config-directory))))
     (when (uiop:directory-exists-p previous-config-directory)
-      (l:info :radiance.migrate "Migrating previous configuration from ~a."
+      (l:info :radiance.migration "Migrating previous configuration from ~a."
               previous-config-directory)
       (loop for original-path in (uiop:subdirectories previous-config-directory)
-            for environment = (car (last (pathname-directory environment)))
+            for environment = (car (last (pathname-directory original-path)))
             for new-path = (environment-directory environment :configuration)
-            do (rename-file original-path new-path))
+            do (when (uiop:directory-exists-p new-path)
+                 (rename-file new-path
+                              (make-pathname :directory (append (butlast (pathname-directory new-path))
+                                                                (list (format NIL "before-migration-~a" environment)))
+                                             :defaults new-path)))
+               (rename-file original-path new-path))
       (uiop:delete-empty-directory previous-config-directory))))
