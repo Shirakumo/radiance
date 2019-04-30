e49ce58b9e2c61622cb9bab12b2611a6d45dc9f8
Harald Judt <h.judt@gmx.at>
2019-04-30 08:44:49 +0200

    radiance-test: Split up request API endpoints and test raw requests
    This tests the new body-stream capabilities of the server
    implementations. We need to track the streams separately because they
    will get closed after they have been processed by the API endpoint.
    
    Also deactivate dexador connection pooling to avoid simple-stream
    errors caused by closed sockets (at least reproducible in sbcl).

--- test/server.lisp
+++ test/server.lisp
@@ -10,17 +10,44 @@ Author: Harald Judt <h.judt@gmx.at>
 sequentially, so no need to protect it against multiple simultaneous
 accesses.")
 
-(defparameter *test-uri* "http://localhost:8080/api/test/request")
+(defparameter *body-streams* nil
+  "Holds processed body-streams of *requests*. It is necessary to keep
+them separately because the body-streams are only available during the
+API endpoint processing, and will have been already closed when
+testing.")
 
-(radiance:define-api test/request () ()
+(defparameter *get-uri* "http://localhost:8080/api/test/request/get")
+
+(defparameter *post-uri* "http://localhost:8080/api/test/request/post")
+
+(defparameter *raw-uri* "http://localhost:8080/api/test/request/raw")
+
+(radiance:define-api test/request/get () ()
   "API endpoint to send test requests to for later analysis."
-  (push radiance:*request* *requests*)
-  (radiance:api-output nil))
+  (push (radiance:*request*) *requests*)
+  (radiance:api-output "GET request received."))
+
+(radiance:define-api test/request/post () ()
+  "API endpoint to send POST test requests to for later analysis."
+  (push (radiance:*request*) *requests*)
+  (radiance:api-output "POST request received."))
+
+(radiance:define-api test/request/raw () ()
+  "API endpoint to send POST test requests to for later analysis of
+their body-streams."
+  (let ((request (radiance:*request*)))
+    (push request *requests*)
+    (push
+     (loop with stream = (radiance:body-stream request)
+           for byte = (read-byte stream nil)
+           while byte collect byte)
+     *body-streams*))
+  (radiance:api-output "POST request with body-stream received."))
 
 (defun get-uri (&optional get-params)
   "Make a URI from get-params, which need to be provided as an alist."
   (if get-params
-      (loop with uri = (uiop:strcat *test-uri* "?")
+      (loop with uri = (uiop:strcat *get-uri* "?")
             with count = (length get-params)
             for pos from 1
             for (key . val) in get-params do
@@ -28,7 +55,7 @@ accesses.")
             while (< pos count) do
               (setq uri (uiop:strcat uri "&"))
             finally (return uri))
-      *test-uri*))
+      *get-uri*))
 
 (defun ht-entry-equals (hash-table key val)
   "Checks whether key in hash-table exists and its value has been set
@@ -54,9 +81,11 @@ to val."
 (define-test get-requests
   :parent requests
   (setf *requests* nil)
-  (dex:get (get-uri))
-  (dex:get (get-uri '(("user" . "john_doe"))))
-  (dex:get (get-uri '(("user" . "john_doe")("mail" . "abc@hotmail.com"))))
+  (dex:get (get-uri) :use-connection-pool nil :keep-alive nil)
+  (dex:get (get-uri '(("user" . "john_doe")))
+           :use-connection-pool nil :keep-alive nil)
+  (dex:get (get-uri '(("user" . "john_doe")("mail" . "abc@hotmail.com")))
+           :use-connection-pool nil :keep-alive nil)
   (destructuring-bind (req1 req2 req3)
       (nreverse *requests*)
     (of-type radiance:request req1)
@@ -100,10 +129,12 @@ to val."
 (define-test post-requests
   :parent requests
   (setf *requests* nil)
-  (dex:post *test-uri*)
-  (dex:post *test-uri* :content '(("user" . "john_doe")))
-  (dex:post *test-uri* :content '(("user" . "john_doe")
-                                  ("mail" . "abc@hotmail.com")))
+  (dex:post *post-uri* :use-connection-pool nil :keep-alive nil)
+  (dex:post *post-uri* :content '(("user" . "john_doe"))
+                       :use-connection-pool nil :keep-alive nil)
+  (dex:post *post-uri* :content '(("user" . "john_doe")
+                                  ("mail" . "abc@hotmail.com"))
+                       :use-connection-pool nil :keep-alive nil)
   (destructuring-bind (req1 req2 req3)
       (nreverse *requests*)
     (of-type radiance:request req1)
@@ -145,3 +176,32 @@ to val."
       (true (ht-entry-equals post2 "user" "john_doe"))
       (true (ht-entry-equals post3 "user" "john_doe"))
       (true (ht-entry-equals post3 "mail" "abc@hotmail.com")))))
+
+(define-test raw-post-requests
+  :parent requests
+  :fix (*body-streams*)
+  (setf *requests* nil)
+  (setf *body-streams* nil)
+  (let ((json "{ \"user\": \"john_doe\", \"mail:\" \"abc@hotmail.com\" }"))
+    (dex:post *raw-uri*
+              :headers '(("content-type" . "application/json"))
+              :content json
+              :use-connection-pool nil
+              :keep-alive nil)
+    (destructuring-bind (req1)
+        (nreverse *requests*)
+      (destructuring-bind (body1)
+          (nreverse *body-streams*)
+        (of-type radiance:request req1)
+        (is equal :post (radiance:http-method req1))
+        (true (stringp (radiance:domain req1)))
+        (true (integerp (radiance:issue-time req1)))
+        (is > 0 (radiance:issue-time req1))
+        (true (remote-addr-p (radiance:remote req1)))
+        (of-type radiance:uri (radiance:uri req1))
+        (true (string= (radiance:user-agent req1) dexador.util:*default-user-agent*))
+        (true (string= (radiance:header "content-type" req1)
+                       "application/json"))
+        (true (radiance:body-stream req1))
+        (is equal (hash-table-count (radiance:post-data req1)) 0)
+        (is equal json (flexi-streams:octets-to-string body1))))))
